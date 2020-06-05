a578a590e407f2a04596a57146d01fea8e829527
sukumar Raghavan <sukumar.raghavan@datarobot.com>
2020-06-06 07:56:30 +0800

    [RAPTOR-2814] adding tests for Keras custom training model template (#8)
    * [RAPTOR-2814] adding tests
    
    * [RAPTOR-2814] fix test and minor changes in keras template
    
    * [RAPTOR-2814] remove default arg
    
    * [RAPTOR-2814] minor update
    
    * [RAPTOR-2814] test fix
    
    * [RAPTOR-2814] test fix
    
    * [RAPTOR-2814] test fix
    
    * [RAPTOR-2814] skip predict
    
    * [RAPTOR-2814] test fix
    
    * [RAPTOR-2814] test fix
    
    * update test
    
    * remove gradle artifacts
    
    Co-authored-by: Yakov Goldberg <yakov.goldberg@datarobot.com>

--- public_dropin_environments/python3_keras/fit.sh
+++ public_dropin_environments/python3_keras/fit.sh
@@ -8,7 +8,7 @@ export y="${INPUT_DIRECTORY}/y.csv"
 export weights="${INPUT_DIRECTORY}/weights.csv"
 
 CMD="drum fit --input ${X} --target-csv ${y} --num-rows ALL --output ${ARTIFACT_DIRECTORY} \
---code-dir ${CODEPATH} --verbose"
+--code-dir ${CODEPATH} --skip-predict --verbose"
 
 if [ -n "${POSITIVE_CLASS_LABEL}" ]; then
     CMD="${CMD} --negative-class-label ${NEGATIVE_CLASS_LABEL} \
