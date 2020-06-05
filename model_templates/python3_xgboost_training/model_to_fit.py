a9042d23956805ea191f602db62b6d59d9600c4e
sukumar Raghavan <sukumar.raghavan@datarobot.com>
2020-06-05 15:09:43 +0800

    [RAPTOR-2798] XGBoost custom model template added (#3)
    * [RAPTOR-2798] adding XGBoost model template with test
    
    * [RAPTOR-2798] adding XGBoost model template with test
    
    * [RAPTOR-2798] tested and fixed commit
    
    * [RAPTOR-2798] binary classification and regression code
    
    * [RAPTOR-2798] blackify
    
    * [RAPTOR-2798] adding comments
    
    * [RAPTOR-2798] unused import removed
    
    * [RAPTOR-2798] updating comments
    
    * [RAPTOR-2798] changes to support both native and sklearn api wrapper for xgboost
    
    * [RAPTOR-2798] black
    
    * [RAPTOR-2798] test fix
    
    * split training/inference model templates
    
    * fix test
    
    * fix test 2
    
    * black
    
    Co-authored-by: Yakov Goldberg <yakov.goldberg@datarobot.com>

---
+++ model_templates/python3_xgboost_training/model_to_fit.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+
+from xgboost import XGBClassifier, XGBRegressor
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import pandas as pd
+
+
+def create_regression_model() -> XGBRegressor:
+    """
+    Create a regression model.
+
+    Returns
+    -------
+    XGBRegressor
+        XGBoost regressor model
+    """
+    xg_reg = XGBRegressor(
+        objective="reg:squarederror",
+        colsample_bytree=0.5,
+        learning_rate=0.1,
+        max_depth=5,
+        alpha=10,
+        n_estimators=50,
+        seed=123,
+    )
+    return xg_reg
+
+
+def create_binary_classification_model() -> XGBClassifier:
+    """
+    Create a binary classification model.
+
+    Returns
+    -------
+    XGBClassifier
+        XGBoost classifier model
+    """
+    xg_clf = XGBClassifier(
+        objective="binary:logistic",
+        colsample_bytree=0.5,
+        learning_rate=0.1,
+        max_depth=5,
+        alpha=10,
+        n_estimators=50,
+        seed=123,
+    )
+    return xg_clf
+
+
+def make_classifier_pipeline(X: pd.DataFrame) -> Pipeline:
+    """
+    Make the classifier pipeline with the required preprocessor steps and estimator in the end.
+
+    Parameters
+    ----------
+    X
+        X containing all the required features for training
+
+    Returns
+    -------
+    Pipeline
+        Classifier pipeline with preprocessor and estimator
+    """
+    numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
+    num_features = list(X.select_dtypes(include=numerics).columns)
+
+    # This example model only uses numeric features and drops the rest
+    num_transformer = Pipeline(
+        steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler()),]
+    )
+
+    preprocessor = ColumnTransformer(transformers=[("num", num_transformer, num_features),])
+
+    # create model
+    estimator = create_binary_classification_model()
+
+    # pipeline with preprocessor and estimator bundled
+    classifier_pipeline = Pipeline(steps=[("preprocessor", preprocessor), ("estimator", estimator)])
+    return classifier_pipeline
+
+
+def make_regressor_pipeline(X: pd.DataFrame) -> Pipeline:
+    """
+    Make the regressor pipeline with the required preprocessor steps and estimator in the end.
+
+    Parameters
+    ----------
+    X
+        X containing all the required features for training
+
+    Returns
+    -------
+    Pipeline
+        Regressor pipeline with preprocessor and estimator
+    """
+    numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
+    num_features = list(X.select_dtypes(include=numerics).columns)
+
+    # This example model only uses numeric features and drops the rest
+    num_transformer = Pipeline(
+        steps=[("imputer", SimpleImputer(strategy="mean")), ("standardize", StandardScaler())]
+    )
+    preprocessor = ColumnTransformer(transformers=[("num", num_transformer, num_features),])
+
+    # create model
+    estimator = create_regression_model()
+
+    # pipeline with preprocessor and estimator bundled
+    regressor_pipeline = Pipeline(steps=[("preprocessor", preprocessor), ("estimator", estimator)])
+    return regressor_pipeline
