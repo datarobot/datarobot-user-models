7271a58f47262ba74023e24ab77171cd32f70b03
Pavel Ustinov <pavel.ustinov@datarobot.com>
2020-06-13 14:17:23 +0200

    [RAPTOR-2839] modify values for PMML tests

--- tests/drum/test_custom_model.py
+++ tests/drum/test_custom_model.py
@@ -173,7 +173,7 @@ class TestCMRunner:
             (KERAS, BINARY): ["Iris-setosa", "Iris-versicolor"],
             (KERAS_VIZAI_TRAINING_JOBLIB, VIZAI_BINARY): ["cats", "dogs"],
             (RDS, BINARY): ["Iris-setosa", "Iris-versicolor"],
-            (PYPMML, PYPMML_BINARY): ["Iris-setosa", "Iris-versicolor"],
+            (PYPMML, BINARY): ["Iris-setosa", "Iris-versicolor"],
         }
 
     @classmethod
