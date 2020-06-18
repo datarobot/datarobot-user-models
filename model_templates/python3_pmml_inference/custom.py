b00b362fe81e1e3d2b99ecd841864e7e58f84b73
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-06-17 21:01:56 -0700

    [RAPTOR-2839] add PMML inference model template and dropin env (#32)
    * [RAPTOR-2839] add PMML inference model template and dropin env
    
    * fix readme
    
    * add func tests
    
    * bump version to rc2
    
    * make all the envs use drum 1.1.2rc2
    
    * make scritps executable

---
+++ model_templates/python3_pmml_inference/custom.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def transform(data, model):
+    """
+    Note: This hook may not have to be implemented for your model.
+    In this case implemented for the model used in the example.
+
+    Modify this method to add data transformation before scoring calls. For example, this can be
+    used to implement one-hot encoding for models that don't include it on their own.
+
+    Parameters
+    ----------
+    data: pd.DataFrame
+    model: object, the deserialized model
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+    # Execute any steps you need to do before scoring
+    # Remove target columns if  they're in the dataset
+    data = data.fillna(0)
+
+    return data
