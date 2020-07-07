f61053cd633dcafe13bc62be52037ac4da972e34
Roman Vorobii <roman.vorobii@datarobot.com>
2020-07-07 19:48:48 +0300

    Tests

--- tests/drum/test_custom_model.py
+++ tests/drum/test_custom_model.py
@@ -23,9 +23,11 @@ from datarobot_drum.drum.common import (
     CustomHooks,
     ArgumentsOptions,
     PythonArtifacts,
+    RunMode,
 )
 
 from datarobot_drum.drum.runtime import DrumRuntime
+from datarobot_drum.drum.args_parser import CMRunnerArgsRegistry
 
 # Framweork keywords
 XGB = "xgboost"
@@ -775,7 +777,9 @@ class TestDrumRuntime:
         TestCMRunner.setup_class()
 
     Options = collections.namedtuple(
-        "Options", "force_start_internal address", defaults=["localhost"]
+        "Options",
+        "force_start_internal {} address".format(CMRunnerArgsRegistry.SUBPARSER_DEST_KEYWORD),
+        defaults=[RunMode.SERVER, "localhost"],
     )
 
     @mock.patch("datarobot_drum.drum.runtime.run_error_server")
@@ -799,6 +803,16 @@ class TestDrumRuntime:
             with DrumRuntime() as runtime:
                 runtime.options = TestDrumRuntime.Options(False)
                 runtime.initialization_succeeded = True
+                raise ValueError()
+
+        mock_run_error_server.assert_not_called()
+
+    @mock.patch("datarobot_drum.drum.runtime.run_error_server")
+    def test_exception_not_server_mode(self, mock_run_error_server):
+        with pytest.raises(ValueError):
+            with DrumRuntime() as runtime:
+                runtime.options = TestDrumRuntime.Options(False, RunMode.SCORE)
+                runtime.initialization_succeeded = False
                 raise ValueError()
 
         mock_run_error_server.assert_not_called()
