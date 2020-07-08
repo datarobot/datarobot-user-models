cbfd574468c2c12817dc8d3a9130a36326ee64bf
Roman Vorobii <roman.vorobii@datarobot.com>
2020-07-08 16:13:28 +0300

    Use stub exception in DrumRuntime unit tests

--- tests/drum/test_custom_model.py
+++ tests/drum/test_custom_model.py
@@ -784,6 +784,9 @@ class TestDrumRuntime:
         defaults=[RunMode.SERVER, None, "localhost"],
     )
 
+    class StubDrumException(Exception):
+        pass
+
     @mock.patch("datarobot_drum.drum.runtime.run_error_server")
     def test_no_exceptions(self, mock_run_error_server):
         with DrumRuntime():
@@ -793,59 +796,59 @@ class TestDrumRuntime:
 
     @mock.patch("datarobot_drum.drum.runtime.run_error_server")
     def test_exception_no_options(self, mock_run_error_server):
-        with pytest.raises(ValueError):
+        with pytest.raises(TestDrumRuntime.StubDrumException):
             with DrumRuntime():
-                raise ValueError()
+                raise TestDrumRuntime.StubDrumException()
 
         mock_run_error_server.assert_not_called()
 
     @mock.patch("datarobot_drum.drum.runtime.run_error_server")
     def test_exception_initialization_succeeded(self, mock_run_error_server):
-        with pytest.raises(ValueError):
+        with pytest.raises(TestDrumRuntime.StubDrumException):
             with DrumRuntime() as runtime:
                 runtime.options = TestDrumRuntime.Options(False)
                 runtime.initialization_succeeded = True
-                raise ValueError()
+                raise TestDrumRuntime.StubDrumException()
 
         mock_run_error_server.assert_not_called()
 
     @mock.patch("datarobot_drum.drum.runtime.run_error_server")
     def test_exception_not_server_mode(self, mock_run_error_server):
-        with pytest.raises(ValueError):
+        with pytest.raises(TestDrumRuntime.StubDrumException):
             with DrumRuntime() as runtime:
                 runtime.options = TestDrumRuntime.Options(False, RunMode.SCORE)
                 runtime.initialization_succeeded = False
-                raise ValueError()
+                raise TestDrumRuntime.StubDrumException()
 
         mock_run_error_server.assert_not_called()
 
     @mock.patch("datarobot_drum.drum.runtime.run_error_server")
     def test_exception_not_server_mode(self, mock_run_error_server):
-        with pytest.raises(ValueError):
+        with pytest.raises(TestDrumRuntime.StubDrumException):
             with DrumRuntime() as runtime:
                 runtime.options = TestDrumRuntime.Options(False, RunMode.SERVER, "path_to_image")
                 runtime.initialization_succeeded = False
-                raise ValueError()
+                raise TestDrumRuntime.StubDrumException()
 
         mock_run_error_server.assert_not_called()
 
     @mock.patch("datarobot_drum.drum.runtime.run_error_server")
     def test_exception_no_force_start(self, mock_run_error_server):
-        with pytest.raises(ValueError):
+        with pytest.raises(TestDrumRuntime.StubDrumException):
             with DrumRuntime() as runtime:
                 runtime.options = TestDrumRuntime.Options(False)
                 runtime.initialization_succeeded = False
-                raise ValueError()
+                raise TestDrumRuntime.StubDrumException()
 
         mock_run_error_server.assert_not_called()
 
     @mock.patch("datarobot_drum.drum.runtime.run_error_server")
     def test_exception_force_start(self, mock_run_error_server):
-        with pytest.raises(ValueError):
+        with pytest.raises(TestDrumRuntime.StubDrumException):
             with DrumRuntime() as runtime:
                 runtime.options = TestDrumRuntime.Options(True)
                 runtime.initialization_succeeded = False
-                raise ValueError()
+                raise TestDrumRuntime.StubDrumException()
 
         mock_run_error_server.assert_called()
 
