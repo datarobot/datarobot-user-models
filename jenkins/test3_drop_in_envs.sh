b9f52f9c33bd9fbb5b8b692743d88461c0ec863b
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-07-17 15:51:16 -0700

    [RAPTOR-3153] install requirements file from the jarvis triggered script (#79)
    split requirements_dev

--- jenkins/test3_drop_in_envs.sh
+++ jenkins/test3_drop_in_envs.sh
@@ -2,6 +2,7 @@
 
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
+pip install -r requirements_test.txt
 py.test tests/functional/test_drop_in_environments.py \
         tests/functional/test_inference_model_templates.py \
         tests/functional/test_training_model_templates.py \
