7d0aa04e386c2623294a41c50573339469af3253
Timothy Whittaker <timothy.whittaker@timothy.whittaker.MacBook>
2020-07-21 16:50:43 -0400

    add h2opredictor. update makefile. update pom dependencies
    update drum/common.py
    
    update java_predictor/customer_scoring_code.py
    
    add h2o mojo inference models
    
    add h2o pojo inference models
    
    update README

---
+++ custom_model_runner/datarobot_drum/resource/components/Python/java_predictor/src/main/scala/com/datarobot/custom/H2OPredictor.scala
@@ -0,0 +1,192 @@
+package com.datarobot.custom
+
+// h2o dependencies
+import hex.genmodel.GenModel
+import hex.genmodel.easy.EasyPredictModelWrapper;
+import hex.genmodel.easy.RowData;
+import hex.genmodel.MojoModel;
+
+import scala.util.{Try, Success, Failure}
+import collection.JavaConverters._
+
+import java.util.ServiceLoader
+import java.util.ArrayList
+import java.net.URLClassLoader;
+import java.lang.Thread
+import java.io.File
+import java.io.{BufferedReader, StringReader, StringWriter}
+import java.nio.file.Paths
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+class H2OPredictor(
+    name: String
+) extends BasePredictor(name) {
+
+  var model: EasyPredictModelWrapper = null
+  var params: java.util.HashMap[String, Any] = null
+  var isRegression: Boolean = false
+
+  var customModelPath: String = null
+  var negativeClassLabel: String = null
+  var positiveClassLabel: String = null
+
+  def predict(stringCSV: String): String = {
+
+    val csvPrinter: CSVPrinter = this.isRegression match {
+      case true =>
+        new CSVPrinter(
+          new StringWriter(),
+          CSVFormat.DEFAULT.withHeader("Predictions")
+        )
+      case false =>
+        new CSVPrinter(
+          new StringWriter(),
+          CSVFormat.DEFAULT
+            .withHeader(this.positiveClassLabel, this.negativeClassLabel)
+        )
+    }
+
+    val predictions = Try(this.scoreStringCSV(stringCSV))
+
+    predictions match {
+      case Success(preds) => {
+        isRegression match {
+          case true =>
+            preds.foreach(p =>
+              csvPrinter.printRecord(p(0).asInstanceOf[Object])
+            )
+          case false =>
+            preds.foreach { p =>
+              csvPrinter.printRecord(
+                p(1).asInstanceOf[Object],
+                p(0).asInstanceOf[Object]
+              )
+            }
+        }
+      }
+      case Failure(e) => throw new Exception(e)
+    }
+
+    csvPrinter.flush()
+    val outStream: StringWriter =
+      csvPrinter.getOut().asInstanceOf[StringWriter];
+    outStream.toString()
+
+  }
+
+  def scoreStringCSV(stringCSV: String) = {
+
+    val csvFormat = CSVFormat.DEFAULT.withHeader();
+
+    val parser =
+      csvFormat.parse(new BufferedReader(new StringReader(stringCSV)))
+
+    val sParser = parser.iterator.asScala.map { _.toMap }.map { map2RowData }
+
+    val predictions = sParser.map { record =>
+      val prediction = this.isRegression match {
+        case true  => Array(this.model.predictRegression(record).value)
+        case false => this.model.predictBinomial(record).classProbabilities
+      }
+      prediction
+    }.toArray
+    predictions
+  }
+
+  def map2RowData(x: java.util.Map[String, String]): RowData = {
+    val row = x.asScala
+    val rowData: RowData = new RowData();
+    row.foreach { case (k, v) => rowData.put(k, v.toString) }
+    rowData
+  }
+
+  def modelConfigViaMojo(mojo: java.io.File): EasyPredictModelWrapper.Config = {
+    new EasyPredictModelWrapper.Config()
+      .setModel(MojoModel.load(mojo.getAbsolutePath))
+      .setConvertUnknownCategoricalLevelsToNa(true)
+      .setConvertInvalidNumbersToNa(true)
+  }
+
+  // this would work with a properly compiled / packaged pojo
+  // this will work with a compile pojo
+  // the expectation is that we compile the pojo when the container is spun up.
+// s"javac -cp code/h2o-genmodel-3.30.0.6.jar ${pojo.getAbsolutePath}".!
+
+  def modelConfigViaPojo(
+      pojo: java.io.File
+  ): EasyPredictModelWrapper.Config = {
+    val urls = pojo.getName.endsWith("java") match {
+      case true => {
+        Array(pojo.getAbsoluteFile.getParentFile.toURI.toURL)
+      }
+      case false => Array(pojo.toURI.toURL)
+    }
+
+    val pojoName = pojo.getName.replace(".jar", "").replace(".java", "")
+    val urlClassLoader = URLClassLoader.newInstance(
+      urls,
+      Thread.currentThread().getContextClassLoader()
+    )
+    val h2oRawPredictor =
+      urlClassLoader.loadClass(pojoName).newInstance.asInstanceOf[GenModel]
+    val modelConfig = new EasyPredictModelWrapper.Config()
+      .setModel(h2oRawPredictor)
+      .setConvertUnknownCategoricalLevelsToNa(true)
+      .setConvertInvalidNumbersToNa(true)
+    // EasyPredictModelWrapper = new EasyPredictModelWrapper(modelConfig)
+    modelConfig
+  }
+
+  def loadModel(modelDir: String): EasyPredictModelWrapper = {
+
+    val re = new scala.util.matching.Regex("(.java$)|(.jar$)|(.zip$)")
+
+    val files = Paths.get(modelDir).toFile().listFiles().filter { f =>
+      re.findAllMatchIn(f.getName).hasNext
+    }
+
+    val file = files.length match {
+      case 0 =>
+        throw new Exception("no model artifact found in model directory")
+      case 1 => files.apply(0)
+      case _ =>
+        throw new Exception(
+          s"more than 1 main model artifact found in model directory: ${files.map { _.getName }.mkString(",")}"
+        )
+    }
+
+    val fileExt = re.findAllIn(file.getName).next
+
+    val modelConfig = fileExt match {
+      case ".zip" => modelConfigViaMojo(file)
+      case _      => modelConfigViaPojo(file)
+    }
+
+    val model = new EasyPredictModelWrapper(modelConfig)
+
+    model
+  }
+
+  def configure(
+      params: java.util.Map[String, Any] = new java.util.HashMap[String, Any]()
+  ) = {
+
+    model = loadModel(
+      params.get("__custom_model_path__").asInstanceOf[String]
+    )
+    customModelPath = params.get("__custom_model_path__").asInstanceOf[String]
+    negativeClassLabel = params.get("negativeClassLabel").asInstanceOf[String]
+    positiveClassLabel = params.get("positiveClassLabel").asInstanceOf[String]
+
+    isRegression = model.m.getModelCategory match {
+      case hex.ModelCategory.Regression => true
+      case hex.ModelCategory.Binomial   => false
+      case _ =>
+        throw new Exception("model is not regression or binary classification")
+    }
+
+  }
+
+}
---
+++ model_templates/inference/h2o_mojo/README.md
@@ -0,0 +1,31 @@
+## H2O Mojo Template
+
+__WIP__
+__TESTS ARE COMING__
+
+These models are inteded to work with the [Java Drop-In Environment](../../../public_dropin_environments/java_codegen/).
+
+The models provided in each folder are H2O models exported as MOJOs.  For more details see [H2O MOJO](http://docs.h2o.ai/h2o/latest-stable/h2o-docs/productionizing.html)
+
+├── binary
+│   └── XGBoost_grid__1_AutoML_20200717_163214_model_159.zip
+└── regression
+    └── drf_887c2e5b_0941_40b7_ae26_cae274c4b424.zip
+
+
+### Instructions
+Upload the a H2O Mojo file as the only file in the custom model and use the Java Drop-In Environment with it. 
+
+### Examples
+
+* Binary - The binary example is based on the iris dataset
+* regression - boston housing pricing dataset with target `MEDV`. 
+
+### To run locally using 'drum'
+Paths are relative to `./datarobot-user-models/model_templates/inference`:  
+
+#### Binary 
+`drum score --code-dir ./h2o_mojo/binary --input ../../tests/testdata/iris_binary_training.csv --positive-class-label 1 --negative-class-label 0`
+
+#### Regression 
+`drum score --code-dir ./h2o_mojo/regression --input ../../tests/testdata/boston_housing.csv`
