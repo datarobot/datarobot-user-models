5294b7fce6c1506c956cdec6e9160564f9713aef
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-07-22 13:14:10 -0700

    Update drum_integration_tests_base docker (#86)
    JDK is required to compile Scala code.

--- docker/drum_integration_tests_base/build_test_docker.sh
+++ docker/drum_integration_tests_base/build_test_docker.sh
@@ -3,6 +3,8 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 TEST_DOCKER_DIR=$GIT_ROOT/docker/drum_integration_tests_base
 
+IMAGE_NAME=datarobot/drum_integration_tests_base
+
 echo "GIT_ROOT: $GIT_ROOT"
 echo "TEST_DOCKER_DIR: $TEST_DOCKER_DIR"
 
@@ -17,5 +19,8 @@ ls -la
 echo
 echo
 echo "Building docker image for drum tests"
-docker build -t datarobot/drum_integration_tests_base ./
+docker build -t $IMAGE_NAME ./
 
+# Image is ready at this moment, but run make on drum to pull in all the java deps and commit the image
+docker run -t -v $GIT_ROOT/custom_model_runner:/tmp/drum datarobot/drum_integration_tests_base bash -c "cd /tmp/drum && make"
+docker commit `docker ps -lq` $IMAGE_NAME
