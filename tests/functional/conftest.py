65b42dd9aeab33cad0bb44666e9ae47418b140db
Timothy Whittaker <timothy.whittaker@datarobot.com>
2020-07-30 12:06:41 -0400

    * fix fixtures * black

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -72,13 +72,6 @@ def java_drop_in_env():
 
 
 @pytest.fixture(scope="session")
-def java_drop_in_env():
-    env_dir = os.path.join(BASE_TEMPLATE_ENV_DIR, "java_h2o")
-    environment = dr.ExecutionEnvironment.create(name="java_h2o_drop_in")
-    environment_version = dr.ExecutionEnvironmentVersion.create(environment.id, env_dir)
-    return environment.id, environment_version.id
-
-@pytest.fixture(scope="session")
 def java_h2o_drop_in_env():
     env_dir = os.path.join(BASE_TEMPLATE_ENV_DIR, "java_h2o")
     environment = dr.ExecutionEnvironment.create(name="java_h2o_drop_in")
