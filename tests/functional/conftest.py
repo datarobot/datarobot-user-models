b232a29f76c51b9f23de41cd57fc06dadf32b287
Timothy Whittaker <timothy.whittaker@datarobot.com>
2020-07-30 20:44:42 -0400

    black

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -78,6 +78,7 @@ def java_h2o_drop_in_env():
     environment_version = dr.ExecutionEnvironmentVersion.create(environment.id, env_dir)
     return environment.id, environment_version.id
 
+
 @pytest.fixture(scope="session")
 def sklearn_drop_in_env():
     env_dir = os.path.join(BASE_TEMPLATE_ENV_DIR, "python3_sklearn")
