76326d9981a1cb1e29ef3bbb6da69b4107e93b7b
Roman Vorobii <roman.vorobii@datarobot.com>
2020-08-27 20:55:40 +0300

    Black

--- custom_model_runner/datarobot_drum/drum/model_adapter.py
+++ custom_model_runner/datarobot_drum/drum/model_adapter.py
@@ -150,10 +150,7 @@ class PythonModelAdapter:
                 "They support filenames with the following extensions {}.\n"
                 "If your artifact is not supported by default predictor, implement custom.load_model hook.\n"
                 "List of retrieved files are: {}".format(
-                    RUNNING_LANG_MSG,
-                    self._model_dir,
-                    all_supported_extensions,
-                    files_list_str,
+                    RUNNING_LANG_MSG, self._model_dir, all_supported_extensions, files_list_str
                 )
             )
 
