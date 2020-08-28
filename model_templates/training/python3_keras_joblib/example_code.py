563b9fdaf6d2326630b636829065a650311ebf8e
Jon Polit <jon.polit@datarobot.com>
2020-08-28 11:25:27 -0400

    run black

--- model_templates/training/python3_keras_joblib/example_code.py
+++ model_templates/training/python3_keras_joblib/example_code.py
@@ -98,7 +98,7 @@ def make_classifier_pipeline(X: pd.DataFrame) -> Pipeline:
     """
     numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
     # exclude any completely-missing columns when checking for numerics
-    num_features = list(X.dropna(axis=1, how='all').select_dtypes(include=numerics).columns)
+    num_features = list(X.dropna(axis=1, how="all").select_dtypes(include=numerics).columns)
 
     # This example model only uses numeric features and drops the rest
     num_transformer = Pipeline(steps=[("imputer", SimpleImputer(strategy="mean"))])
