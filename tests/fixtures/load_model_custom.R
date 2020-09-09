623efc3b93a5e148f332bb7fa0519b3c4fd0adfa
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-09-08 21:57:56 -0700

    [RAPTOR-2697] fix handling NA values in R

--- tests/fixtures/load_model_custom.R
+++ tests/fixtures/load_model_custom.R
@@ -7,3 +7,8 @@ load_model <- function(input_dir) {
     file.path(input_dir, "r_bin.rds")
     readRDS(file.path(input_dir, "r_bin.rds"))
 }
+
+transform <- function(data, model) {
+    data[is.na(data)] <- 0
+    data
+}
