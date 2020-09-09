b433a69100177f09acd19aa68bf12796c4028def
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-09-09 09:57:45 -0700

    [RAPTOR-2697] fix handling NA values in R (#135)

--- custom_model_runner/datarobot_drum/drum/description.py
+++ custom_model_runner/datarobot_drum/drum/description.py
@@ -1,3 +1,3 @@
-version = "1.2.1rc4"
+version = "1.2.1rc5"
 __version__ = version
 project_name = "datarobot-drum"
--- model_templates/inference/r_lang/custom.R
+++ model_templates/inference/r_lang/custom.R
@@ -2,3 +2,8 @@ init <- function(...) {
     library(brnn)
     library(glmnet)
 }
+
+transform <- function(data, model) {
+    data[is.na(data)] <- 0
+    data
+}
