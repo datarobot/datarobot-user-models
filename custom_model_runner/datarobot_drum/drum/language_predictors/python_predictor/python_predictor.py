a5ea9424ba3a8bff64bc19c23b0b6974cef151c9
Lior <lior.amar@datarobot.com>
2020-09-11 15:37:12 -0700

    [RAPTOR-3430] mlops: adding mlops monitoring support (#137)
    * [RAPTOR-3430] mlops: adding mlops monitoring support
    
    * removing forgotten commented code
    
    * review cleanup

--- custom_model_runner/datarobot_drum/drum/language_predictors/python_predictor/python_predictor.py
+++ custom_model_runner/datarobot_drum/drum/language_predictors/python_predictor/python_predictor.py
@@ -1,5 +1,7 @@
 import logging
 import sys
+import pprint
+import time
 
 from datarobot_drum.drum.common import (
     LOGGER_NAME_PREFIX,
@@ -16,6 +18,7 @@ class PythonPredictor(BaseLanguagePredictor):
     def __init__(self):
         super(PythonPredictor, self).__init__()
         self._model_adapter = None
+        self._mlops = None
 
     def configure(self, params):
         super(PythonPredictor, self).configure(params)
@@ -29,10 +32,18 @@ class PythonPredictor(BaseLanguagePredictor):
             raise Exception("Failed to load model")
 
     def predict(self, input_filename):
+
         kwargs = {}
         if self._positive_class_label and self._negative_class_label:
             kwargs[POSITIVE_CLASS_LABEL_ARG_KEYWORD] = self._positive_class_label
             kwargs[NEGATIVE_CLASS_LABEL_ARG_KEYWORD] = self._negative_class_label
 
+        start_predict = time.time()
         predictions = self._model_adapter.predict(input_filename, model=self._model, **kwargs)
+        end_predict = time.time()
+        execution_time_ms = (end_predict - start_predict) * 1000
+
+        # TODO: call monitor only if we are in structured mode
+        self.monitor(input_filename, predictions, execution_time_ms)
+
         return predictions
