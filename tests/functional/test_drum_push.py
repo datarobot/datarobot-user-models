e582b276589b9ae16662a4a468527516d4c67de1
Jon Polit <jon.polit@datarobot.com>
2020-09-16 12:47:16 -0400

    update tests

--- tests/functional/test_drum_push.py
+++ tests/functional/test_drum_push.py
@@ -1,29 +1,64 @@
 from __future__ import absolute_import
 
+import os
 import pytest
+import yaml
 
 from datarobot_drum.drum.common import ArgumentsOptions
-from ..drum.utils import _exec_shell_cmd
+from tests.conftest import PYTHON, SKLEARN, REGRESSION
+from ..drum.utils import _create_custom_model_dir, _exec_shell_cmd
 
 BASE_MODEL_TEMPLATES_DIR = "model_templates"
 BASE_DATASET_DIR = "tests/testdata"
 
 
+def get_push_yaml(env_id, data_path, problem):
+    return """
+            name: drumpush-{problemType}
+            type: training
+            targetType: {problemType}
+            environmentID: {environmentID}
+            validation:
+                input: {dataPath} 
+        """.format(environmentID=env_id,
+                   problemType=problem,
+                   dataPath=data_path
+        )
+
+
 class TestDrumPush(object):
     @pytest.mark.parametrize(
-        # TODO: add more model configs so we can test more things
-        "model_template",
+        "framework, problem, language",
         [
-            "/python3_sklearn",
+            (SKLEARN, REGRESSION, PYTHON),
         ],
     )
     def test_drum_push_training(
         self,
-        model_template,
+        resources,
+        framework,
+        problem,
+        language,
+        tmp_path,
+        sklearn_drop_in_env,
+
     ):
+        custom_model_dir = _create_custom_model_dir(
+            resources,
+            tmp_path,
+            framework,
+            problem,
+            language,
+        )
+
+        env_id, _ = sklearn_drop_in_env
+        yaml_string = get_push_yaml(env_id, resources.datasets(framework, problem), problem)
+        with open(os.path.join(custom_model_dir, 'model-metadata.yaml'), 'w') as outfile:
+            yaml.dump(yaml_string, outfile, default_flow_style=False)
+
         cmd = "{} push --code-dir {} --verbose".format(
             ArgumentsOptions.MAIN_COMMAND,
-            BASE_MODEL_TEMPLATES_DIR + "/training" + model_template,
+            custom_model_dir,
         )
         _exec_shell_cmd(
             cmd, "Failed in {} command line! {}".format(ArgumentsOptions.MAIN_COMMAND, cmd)
