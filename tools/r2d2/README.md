ead56c72428b8d7f7f7956b6a4a8d94dcb060f7d
Lior Amar <lior.amar@datarobot.com>
2020-10-02 14:20:24 -0700

    [RAPTOR-3656] r2d2: a testing inference model
    o r2d2 is a small testing model that is performing actions
      inside the model.
    o r2d2 mimic a regression model
    o expect "cmd" and "arg" column in the input data
    o carry the command in the cmd column with the argument from the "arg" col.
    o actions: memory, exception, timeout.

---
+++ tools/r2d2/README.md
@@ -0,0 +1,94 @@
+# R2D2: A testing inference model
+
+r2d2 inference model provides a quick way to test your environment.
+r2d2 is not really a model, it is a testing program which interpret 
+the prediction requests as actions to execute inside the drum environment.
+
+r2d2 expects is input dataframe to contain the 2 columns: "cmd" and "arg".
+The "cmd" column is supposed to contain the name of the command to run.
+The "arg" column is supposed to contain an argument to pass to the command.
+
+For example, the following csv will cause r2d2 to consume 1500 megabytes 
+of memory 
+    
+    cmd,arg
+    memory, 1500
+
+More info about supported commands can be found in the next
+section.
+
+**Important** info:
+
+* r2d2 only takes the command from the first row in the input data.
+Following rows are ignored.
+* r2d will not fail if input data does not contain the "cmd" and "arg". 
+An error message will be printed to r2d2 stdout, but r2d2 will not fail
+the prediction.
+
+For example the following csv file, will only consume 2000 of memory:
+
+    cmd,arg
+    memory,2000
+    memory,3000
+
+
+## What kind of actions are supported:
+r2d2 supports the following actions:
+* Consume memory
+* Throw exceptions
+* Sleep for a given amount of time, potentially causing timeout.
+
+###Use memory
+This action will cause r2d2 to consume memory. The way to trigger that
+action is to send the "memory" as the command in the "cmd" column of the 
+prediction request. And the total amount of memory to consume in megabytes in
+the "arg" column.
+
+For example, use the following csv file to cause r2d2 consume 1000mb of
+memory:
+
+    cmd,arg
+    memory,1000
+
+### Exception throwing
+This action will cause r2d2 to throw an exception. To do that, send the "cmd"
+column to "exception". Currently the "arg" column is ignored
+
+For example, use the following csv file to cause an exception in predictions.
+(Note: r2d2 will ignore the arg value, 33 in our case).
+
+    cmd,arg
+    exception, 33
+
+### Timeout
+This action will cause r2d2 to sleep for a given amount of seconds. To do that
+set the "cmd" column value to "timeout", and set the "arg" column value to 
+the number of seconds you would like r2d2 to sleep.
+
+For example, use the following csv file to cause a timeout of 10 seconds.
+
+    cmd, arg
+    timeout, 10
+
+
+## How to run r2d2 send predictions in server mode
+First run drum in server mode. For example
+
+    drum server --code-dir ./r2d2/  --address localhost:8999 --verbose
+
+In this example we would like to cause the drum environment to consume
+2000 megabytes (or so) of memory. So prepare a file like the following:
+
+    cmd,arg
+    memory,2000
+    
+Name your csv file memory.csv. 
+
+You can now trigger r2d2 to consume 2000mb of memory by doing:
+
+    curl -X POST --form "X=@./memory.csv" localhost:8999/predict/
+    
+ In the above example, the data in the file _memory.csv_ will be sent
+ to the endpoint "localhost:8999/predict". DRUM will create a dataframe
+ form that file and will forward it to the r2d2 model. The r2d2 will
+ parse the incoming data and will consume 2000mb of memory. 
---
+++ tools/r2d2/custom.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import sys
+import time
+
+prediction_value = None
+
+
+class R2D2:
+    MEGA = 10 ** 6
+    MEGA_STR = ' ' * MEGA
+
+    def __init__(self):
+        self._mem_array = []
+        print("R2D2 __init__")
+
+    def _mem_size_mb(self):
+        return len(self._mem_array)
+
+    def _clear_memory(self):
+        self._mem_array = []
+
+    def _alloc_additional_memory(self, additional_mb):
+        for i in range(0, additional_mb):
+            try:
+                self._mem_array.append(R2D2.MEGA_STR + str(i))
+                print("Adding: {}".format(i))
+            except MemoryError:
+                print("No more memory here..")
+                break
+
+            i += 1
+
+    def alloc_memory(self, arg):
+        # Always taking the info to run from the first command
+        memory_mb = int(arg)
+        print("Found memory_mb in data: {}".format(memory_mb))
+        if memory_mb <= 0:
+            print("Clearing memory")
+            self._clear_memory()
+        elif memory_mb > 0:
+            additional_mem = memory_mb - self._mem_size_mb()
+            if additional_mem > 0:
+                print("Allocating additional {} mb")
+                self._alloc_additional_memory(additional_mem)
+            else:
+                print("No additional memory to allocate, current {} requested {}"
+                      .format(self._mem_size_mb(), memory_mb))
+
+    def raise_exception(self, arg):
+        print("About to raise an exception")
+        raise Exception("Raising exception")
+
+    def consume_time(self, arg):
+        timeout = int(arg)
+        print("About to sleep for {} seconds".format(timeout))
+        time.sleep(timeout)
+
+
+# Global instance
+r2d2 = R2D2()
+
+
+def init(**kwargs):
+    global prediction_value
+    prediction_value = 1
+
+
+def read_input_data(input_file):
+    global prediction_value
+    prediction_value += 1
+    return pd.read_csv(input_file)
+
+
+def load_model(input_dir):
+    global prediction_value
+    prediction_value += 1
+    return "dummy"
+
+
+def transform(data, model):
+    global prediction_value
+    prediction_value += 1
+    return data
+
+
+def score(data, model, **kwargs):
+    global prediction_value
+
+    cmd = data["cmd"][0]
+    arg = data["arg"][0]
+
+    if cmd == "memory":
+        r2d2.alloc_memory(arg)
+    elif cmd == "exception":
+        r2d2.raise_exception(arg)
+    elif cmd == "timeout":
+        r2d2.consume_time(arg)
+    else:
+        print("Cmd: {} is not supported".format(cmd))
+        raise Exception("Bad CMD provided {}".format(cmd))
+
+    print("Done adding memory")
+    prediction_value += 1
+    predictions = pd.DataFrame(
+        [prediction_value for _ in range(data.shape[0])], columns=["Predictions"]
+    )
+    return predictions
+
+
+def post_process(predictions, model):
+    return predictions + 1
+
+
+# Small main for R2D2 usage.
+if __name__ == "__main__":
+    print("Running r2d2 main")
+    print(sys.argv)
+
+    data_mem = pd.DataFrame({"cmd": ["memory"], "arg": [1000]}, columns=["cmd", "arg"])
+    data_exception = pd.DataFrame({"cmd": ["exception"], "arg": [5]}, columns=["cmd", "arg"])
+    data_timeout = pd.DataFrame({"cmd": ["timeout"], "arg": [10]}, columns=["cmd", "arg"])
+
+    print(data_mem)
+    print(data_exception)
+    print(data_timeout)
+
+    init()
+    score(data_mem, None)
+    # This sleep give you some time to check the memory usage using tools like htop/ps
+    time.sleep(30)
+
+    score(data_timeout, None)
+    score(data_exception, None)
