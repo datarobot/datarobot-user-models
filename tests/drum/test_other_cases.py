615c554a9d29e6d984315504e7ce0c206bc96c53
Lior Amar <lior.amar@datarobot.com>
2020-10-08 22:30:42 -0700

    WIP

--- tests/drum/test_other_cases.py
+++ tests/drum/test_other_cases.py
@@ -304,7 +304,8 @@ class TestOtherCases:
             labels=None,
             custom_model_dir=custom_model_dir,
             docker=DOCKER_PYTHON_SKLEARN,
-            memory="500m"
+            memory="500m",
+            fail_on_shutdown_error=False
         ) as run:
             print("r2d2 is running")
             cmd = "python tools/r2d2/custom.py memory 200 --server {}"\
