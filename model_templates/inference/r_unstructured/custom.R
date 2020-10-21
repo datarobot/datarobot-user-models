e299bfde3b0404108c4603ede28ad1f9fed317f8
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-10-20 17:31:15 -0700

    [RAPTOR-3574] model templates for unstructured (#165)

--- model_templates/inference/r_unstructured/custom.R
+++ model_templates/inference/r_unstructured/custom.R
@@ -2,6 +2,7 @@ init <- function(...) {
     library(readr)
     library(stringr)
     library(pack)
+    library(stringi)
 }
 
 load_model <- function(input_dir) {
@@ -17,18 +18,29 @@ numToRawBigEndian <- function(num, nBytes = 4) {
     numRaw
 }
 
-score_unstructured <- function(model, ...) {
+score_unstructured <- function(model, data, query, ...) {
     kwargs <- list(...)
-    if (!is.null(kwargs$data)) {
-        data_text <- stri_conv(kwargs$data, "utf8")
+
+    print(c("Model: ", model))
+    print(c("Incoming content type params: ", kwargs))
+    print(c("Incoming data type: ", typeof(data)))
+    print(paste0("Incoming data: ", paste0(data, collapse=" ")))
+
+    print(c("Incoming query params: ", query))
+
+
+    if (is.raw(data)) {
+        data_text <- stri_conv(data, "utf8")
     } else {
-        data_text <- kwargs$text
+        data_text <- data
     }
     count <- str_count(data_text, " ") + 1
-    if (kwargs$ret_mode == "binary") {
-        ret = list(data=numToRawBigEndian(count))
+    if (!is.null(query) && !is.null(query$ret_mode) && query$ret_mode == "binary") {
+        ret_data = numToRawBigEndian(count)
+        ret_kwargs = list(mimetype="application/octet-stream")
+        ret = list(ret_data, ret_kwargs)
     } else {
-        ret = list(data=toString(count))
+        ret = toString(count)
     }
     ret
 }
