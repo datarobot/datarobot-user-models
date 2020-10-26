17211fa52f43d6c8c263fc6c462aaff75c8ae5cf
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-10-26 09:19:10 -0700

    [RAPTOR-3569] change start_server.sh/fit.sh to take target_type from LRS env var (#186)

--- public_dropin_environments/r_lang/fit.sh
+++ public_dropin_environments/r_lang/fit.sh
@@ -6,34 +6,26 @@ export PYTHONPATH="${CODEPATH}":"${PYTHONPATH}"
 export X="${INPUT_DIRECTORY}/X${TRAINING_DATA_EXTENSION:-.csv}"
 export weights="${INPUT_DIRECTORY}/weights.csv"
 
-CMD="drum fit --input ${X} --num-rows ALL --output ${ARTIFACT_DIRECTORY} \
+CMD="drum fit --target-type ${TARGET_TYPE} --input ${X} --num-rows ALL --output ${ARTIFACT_DIRECTORY} \
 --code-dir ${CODEPATH} --verbose"
 
 if [ -n "${UNSUPERVISED}" ]; then
   CMD="${CMD} --unsupervised "
-  TARGET_TYPE="anomaly"
 else
   export y="${INPUT_DIRECTORY}/y.csv"
   CMD="${CMD} --target-csv ${y}"
-  TARGET_TYPE="regression"
 fi
 
 
 if [ -n "${POSITIVE_CLASS_LABEL}" ]; then
     CMD="${CMD} --negative-class-label ${NEGATIVE_CLASS_LABEL} \
     --positive-class-label ${POSITIVE_CLASS_LABEL}"
-    TARGET_TYPE="regression"
 fi
 if [ -n "${CLASS_LABELS_FILE}" ]; then
     CMD="${CMD} --class-labels-file ${CLASS_LABELS_FILE}"
-    TARGET_TYPE="multiclass"
 fi
 if [ -f "${weights}" ]; then
     CMD="${CMD} --row-weights-csv ${weights}"
-fi
-
-if [ -n "${TARGET_TYPE}" ]; then
-    CMD="${CMD} --target-type ${TARGET_TYPE}"
 fi
 
 echo "${CMD}"
--- public_dropin_environments/r_lang/start_server.sh
+++ public_dropin_environments/r_lang/start_server.sh
@@ -2,23 +2,17 @@
 cd /opt/code/ || exit 1
 export PYTHONPATH=/opt/code
 
-TARGET_TYPE="regression"
-
-CMD="drum server -cd . --address 0.0.0.0:8080 --production --max-workers 1 --show-stacktrace"
+CMD="drum server -cd . --target-type ${TARGET_TYPE} --address 0.0.0.0:8080 --production --max-workers 1 --show-stacktrace"
 
 if [ -n "${POSITIVE_CLASS_LABEL}" ]; then
     CMD="${CMD} --positive-class-label ${POSITIVE_CLASS_LABEL}"
-    TARGET_TYPE="binary"
 fi
 if [ -n "${NEGATIVE_CLASS_LABEL}" ]; then
     CMD="${CMD} --negative-class-label ${NEGATIVE_CLASS_LABEL}"
-    TARGET_TYPE="binary"
 fi
 if [ -n "${CLASS_LABELS_FILE}" ]; then
     CMD="${CMD} --class-labels-file ${CLASS_LABELS_FILE}"
-    TARGET_TYPE="multiclass"
 fi
 
-CMD="${CMD} --target-type ${TARGET_TYPE}"
-
+echo "${CMD}"
 exec ${CMD}
