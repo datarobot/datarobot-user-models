61b0b2e0d525fdd86c496b5a0b7112221dcb8158
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-10-29 13:58:50 -0700

    release v1.4.1
    switch from Flask to uwsgi

--- public_dropin_environments/python3_pmml/start_server.sh
+++ public_dropin_environments/python3_pmml/start_server.sh
@@ -2,7 +2,15 @@
 cd /opt/code/ || exit 1
 export PYTHONPATH=/opt/code
 
-CMD="drum server -cd . --target-type ${TARGET_TYPE} --address 0.0.0.0:8080 --production --max-workers 1 --show-stacktrace"
+CMD="drum server -cd . --target-type ${TARGET_TYPE} --address 0.0.0.0:8080"
+
+# Uncomment the following line to switch from Flask to uwsgi server
+# WITH_UWSGI="1"
+if [ -n "${WITH_UWSGI}" ]; then
+  CMD="${CMD} --production --max-workers 1 --show-stacktrace"
+else
+  CMD="${CMD} --with-error-server"
+fi
 
 if [ -n "${POSITIVE_CLASS_LABEL}" ]; then
     CMD="${CMD} --positive-class-label ${POSITIVE_CLASS_LABEL}"
