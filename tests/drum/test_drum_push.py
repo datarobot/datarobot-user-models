2a0cb4f32e5d4d91674a0ce22c140ff2c28f0ee7
Jon Polit <jon.polit@datarobot.com>
2020-11-02 10:44:25 -0500

    update w anomaly test

--- tests/drum/test_drum_push.py
+++ tests/drum/test_drum_push.py
@@ -5,7 +5,7 @@ import pytest
 import yaml
 
 from datarobot_drum.drum.common import ArgumentsOptions
-from tests.conftest import PYTHON, SKLEARN, REGRESSION, BINARY
+from tests.conftest import PYTHON, REGRESSION, BINARY, ANOMALY, SKLEARN_ANOMALY, SKLEARN_BINARY, SKLEARN_REGRESSION
 from tests.drum.utils import _create_custom_model_dir, _exec_shell_cmd
 
 BASE_MODEL_TEMPLATES_DIR = "model_templates"
@@ -29,8 +29,9 @@ class TestDrumPush(object):
     @pytest.mark.parametrize(
         "framework, problem, language",
         [
-            (SKLEARN, REGRESSION, PYTHON),
-            (SKLEARN, BINARY, PYTHON),
+            (SKLEARN_REGRESSION, REGRESSION, PYTHON),
+            (SKLEARN_BINARY, BINARY, PYTHON),
+            (SKLEARN_ANOMALY, ANOMALY, PYTHON)
         ],
     )
     def test_drum_push_training(
@@ -47,6 +48,7 @@ class TestDrumPush(object):
             framework,
             problem,
             language,
+            is_training=True
         )
 
         env_id= '5e8c889607389fe0f466c72d'
