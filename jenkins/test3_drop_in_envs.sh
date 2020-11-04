ebca6a4df30da2ec4f80fd80d38026982f6a06e6
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-11-04 11:49:32 -0800

    reorder pip installation

--- jenkins/test3_drop_in_envs.sh
+++ jenkins/test3_drop_in_envs.sh
@@ -2,7 +2,9 @@
 
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
-pip install -r requirements_test.txt
 # installing DRUM into the test env is required for push test
 pip install datarobot-drum
+# requirements_test may require newer packages for testing, e.g. `datarobot`
+pip install -r requirements_test.txt
+
 py.test tests/functional/ --junit-xml="$CDIR/results_drop_in.xml"
