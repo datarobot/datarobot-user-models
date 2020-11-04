172fc591a09e0051ed26846bc3d7e80e4a6ca79d
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-11-04 11:06:50 -0800

    install drum

--- jenkins/test3_drop_in_envs.sh
+++ jenkins/test3_drop_in_envs.sh
@@ -3,5 +3,7 @@
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
 pip install -r requirements_test.txt
+# installing DRUM into the test env is required for push test
+pip install datarobot-drum
 py.test tests/functional/test_drum_push.py \
         --junit-xml="$CDIR/results_drop_in.xml"
