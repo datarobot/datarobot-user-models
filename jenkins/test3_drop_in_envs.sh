56d33b587c7623ea63a79b65b8268b72739d8237
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-11-04 11:28:54 -0800

    re-enable all the functional tests

--- jenkins/test3_drop_in_envs.sh
+++ jenkins/test3_drop_in_envs.sh
@@ -5,5 +5,4 @@ CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 pip install -r requirements_test.txt
 # installing DRUM into the test env is required for push test
 pip install datarobot-drum
-py.test tests/functional/test_drum_push.py \
-        --junit-xml="$CDIR/results_drop_in.xml"
+py.test tests/functional/ --junit-xml="$CDIR/results_drop_in.xml"
