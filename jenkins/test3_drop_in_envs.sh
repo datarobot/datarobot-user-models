353759b6ee411416fbd383781f678b0219f11b7d
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-11-16 08:54:18 -0800

    add install pytest-runner

--- jenkins/test3_drop_in_envs.sh
+++ jenkins/test3_drop_in_envs.sh
@@ -2,9 +2,12 @@
 
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
+# need to install this first to workaround quantum requirements
+# and missing argparse-formatter wheel in DR artifactory
+pip install pytest-runner
 # installing DRUM into the test env is required for push test
 pip install datarobot-drum
-# requirements_test may require newer packages for testing, e.g. `datarobot`
+# requirements_test may install newer packages for testing, e.g. `datarobot`
 pip install -r requirements_test.txt
 
 py.test tests/functional/ --junit-xml="$CDIR/results_drop_in.xml"
