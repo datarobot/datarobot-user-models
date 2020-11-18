6e21789776c83b2a37acaab44d1677aeb3d6e9a3
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-11-18 12:57:44 -0800

    [RAPTOR-3993] run functional tests on freshly built drum wheel (#227)
    * [RAPTOR-4004] replace desc formatter with the one provided by argparse
    * [RAPTOR-3784] compare host and container drum versions
    * bump version

---
+++ docker/drum_builder/README.md
@@ -0,0 +1,12 @@
+# Drum builder image
+Repository name: **datarobot/drum-builder**  
+Latest date: 11-16-2020  
+Dockerfile: https://github.com/datarobot/datarobot-user-models/blob/master/docker/drum_builder/Dockerfile
+
+## Description
+This image is used to build DRUM wheel.
+Based on ubuntu:20.04 image. Additionally installed packages required to build DRUM.
+
+## Guidelines
+DataRobot guidelines for publishing images to Docker Hub
+https://datarobot.atlassian.net/wiki/spaces/ENG/pages/927858704/Releasing+Public+Docker+Images+to+Docker+Hub
--- jenkins/test3_drop_in_envs.sh
+++ jenkins/test3_drop_in_envs.sh
@@ -1,13 +1,47 @@
 #!/usr/bin/env bash
 
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+DRUM_BUILDER_IMAGE="datarobot/drum-builder"
+echo $CDIR
 
-# need to install this first to workaround quantum requirements
-# and missing argparse-formatter wheel in DR artifactory
-pip install pytest-runner
+# pull DRUM builder container and build DRUM wheel
+docker pull ${DRUM_BUILDER_IMAGE}
+
+# If we are in terminal will be true when running the script manually. Via Jenkins it will be false
+TERMINAL_OPTION=""
+if [ -t 1 ] ; then
+  TERMINAL_OPTION="-t"
+fi
+
+docker run -i ${TERMINAL_OPTION} --user $(id -u):$(id -g) -v $CDIR:/tmp/drum $DRUM_BUILDER_IMAGE bash -c "cd /tmp/drum/custom_model_runner && make"
+docker rmi $DRUM_BUILDER_IMAGE --force
+
+DRUM_WHEEL=$(find custom_model_runner/dist/datarobot_drum*.whl)
+DRUM_WHEEL_FILENAME=$(basename $DRUM_WHEEL)
+DRUM_WHEEL_REAL_PATH=$(realpath $DRUM_WHEEL)
+
+# Change every environment Dockerfile to install fresly build DRUM wheel
+pushd public_dropin_environments
+DIRS=$(ls)
+for d in $DIRS
+do
+  pushd $d
+  cp $DRUM_WHEEL_REAL_PATH .
+  echo "
+COPY $DRUM_WHEEL_FILENAME $DRUM_WHEEL_FILENAME
+RUN pip3 install -U $DRUM_WHEEL_FILENAME && rm -rf $DRUM_WHEEL_FILENAME" >> Dockerfile
+  popd
+done
+popd
+
 # installing DRUM into the test env is required for push test
-pip install datarobot-drum
+pip install -U $DRUM_WHEEL_REAL_PATH
 # requirements_test may install newer packages for testing, e.g. `datarobot`
 pip install -r requirements_test.txt
 
-py.test tests/functional/ --junit-xml="$CDIR/results_drop_in.xml"
+# put tests in this exact order as they build images and as a result jenkins instance may run out of space
+py.test tests/functional/test_inference_model_templates.py \
+        tests/functional/test_training_model_templates.py \
+        tests/functional/test_drop_in_environments.py \
+        tests/functional/test_drum_push.py \
+        --junit-xml="$CDIR/results_drop_in.xml"
