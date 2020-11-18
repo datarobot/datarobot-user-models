8d4fff8573e7709bb90f309aff3f819c8173ba74
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-11-18 01:17:16 -0800

    tmp

--- jenkins/test3_drop_in_envs.sh
+++ jenkins/test3_drop_in_envs.sh
@@ -10,4 +10,7 @@ pip install datarobot-drum
 # requirements_test may install newer packages for testing, e.g. `datarobot`
 pip install -r requirements_test.txt
 
-py.test tests/functional/ --junit-xml="$CDIR/results_drop_in.xml"
+py.test tests/functional/test_drop_in_environments.py \
+        tests/functional/test_drum_push.py \
+        tests/functional/test_inference_model_templates.py \
+        --junit-xml="$CDIR/results_drop_in.xml"
