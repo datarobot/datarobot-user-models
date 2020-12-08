31fcceba180a55d6afcba7850caa8378ea23d0c0
Jon Polit <jon.polit@datarobot.com>
2020-12-08 16:52:22 -0500

    get rid of a pointless comment

--- tests/fixtures/custom_pred_consistency/custom.py
+++ tests/fixtures/custom_pred_consistency/custom.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import joblib
-from sklearn.linear_model import LogisticRegression  # DELETE
+from sklearn.linear_model import LogisticRegression
 
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
