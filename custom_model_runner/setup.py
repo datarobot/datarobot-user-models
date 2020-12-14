0dc5882f749c15fd1aee3d1545a071ebacc03811
Yakov Goldberg <yakov.goldberg@datarobot.com>
2020-12-14 11:02:02 -0800

    [RAPTOR-3916] make drum require Python >=3.4,<=3.8

--- custom_model_runner/setup.py
+++ custom_model_runner/setup.py
@@ -39,6 +39,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS",
         "Operating System :: POSIX",
@@ -54,5 +55,5 @@ setup(
     scripts=["bin/drum"],
     install_requires=requirements,
     extras_require=extras_require,
-    python_requires=">=3.4",
+    python_requires=">=3.4,<=3.8",
 )
