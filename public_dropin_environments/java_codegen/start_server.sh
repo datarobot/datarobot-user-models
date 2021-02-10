d739f14496eb862f652526147f24cfc19836f508
Yakov Goldberg <yakov.goldberg@datarobot.com>
2021-02-10 00:16:51 -0800

    [RAPTOR-4132] pass start_server.sh arguments to DRUM

--- public_dropin_environments/java_codegen/start_server.sh
+++ public_dropin_environments/java_codegen/start_server.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo "Starting Custom Model environment with DRUM prediction server"
 echo "Environment variables:"
 env
+echo
 
-CMD="drum server"
-echo "Executing: ${CMD}"
+CMD="drum server $@"
+echo "Executing command: ${CMD}"
+echo
 exec ${CMD}
