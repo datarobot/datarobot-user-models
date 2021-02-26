b999ef91b0dbfe578e1b6303220b5cd89b8566bf
Yakov Goldberg <yakov.goldberg@datarobot.com>
2021-02-26 09:00:02 -0800

    address comment

--- custom_model_runner/datarobot_drum/drum/args_parser.py
+++ custom_model_runner/datarobot_drum/drum/args_parser.py
@@ -258,7 +258,7 @@ class CMRunnerArgsRegistry(object):
                 type=are_labels_double_specified,
                 nargs="+",
                 action=RequiredLength,
-                help="The class labels for a multiclass classification case.  The argument can also be provided by setting {} env var. ".format(
+                help="The class labels for a multiclass classification case. The argument can also be provided by setting {} env var. ".format(
                     ArgumentOptionsEnvVars.CLASS_LABELS
                 )
                 + class_label_order_message
@@ -332,7 +332,8 @@ class CMRunnerArgsRegistry(object):
                 "or a directory, containing a Dockerfile, which can be built into a docker image. "
                 "If code dir contains requirements.txt file, DRUM tries to install dependencies during image build. (Reflects the DR App behavior.) "
                 "Requirements installation is supported for Python/R models only. "
-                "Use {} to skip installation.".format(
+                "Use {} to skip installation."
+                "Note: DRUM attempts to install dependencies only if docker context folder is provided, not already built image from the registry.".format(
                     ArgumentsOptions.MAIN_COMMAND,
                     prog_name_lst[1],
                     ArgumentsOptions.SKIP_DEPS_INSTALL,
@@ -929,4 +930,12 @@ class CMRunnerArgsRegistry(object):
                     )
                     exit(1)
 
+        if options.skip_deps_install and options.docker is None:
+            print(
+                "Argument '{}' can only be used together with '{}'.".format(
+                    ArgumentsOptions.SKIP_DEPS_INSTALL,
+                    ArgumentsOptions.DOCKER,
+                )
+            )
+            exit(1)
         CMRunnerArgsRegistry.verify_monitoring_options(options, options.subparser_name)
