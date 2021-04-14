952e614218197d9fc37a1595b381ba3b9c909bb9
Yakov Goldberg <yakov.goldberg@datarobot.com>
2021-04-13 20:48:12 -0700

    fix base test container

--- docker/drum_integration_tests_base/build_test_docker.sh
+++ docker/drum_integration_tests_base/build_test_docker.sh
@@ -22,5 +22,5 @@ echo "Building docker image for drum tests"
 docker build -t $IMAGE_NAME ./
 
 # Image is ready at this moment, but run make on drum to pull in all the java deps and commit the image
-docker run -t --user $(id -u):$(id -g) -v $GIT_ROOT/custom_model_runner:/tmp/drum ${IMAGE_NAME} bash -c "cd /tmp/drum && make"
+docker run -t -v $GIT_ROOT/custom_model_runner:/tmp/drum ${IMAGE_NAME} bash -c ". /opt/v3.7/bin/activate && cd /tmp/drum && make"
 docker commit `docker ps -lq` $IMAGE_NAME
