0b83d2c097bca0f3bde424b818f3d9fdd553ea71
Jesse Bannon <jesse.bannon@datarobot.com>
2021-08-12 14:58:38 -0700

    add simple R template

--- task_templates/transforms/r_transform_recipe/README.md
+++ task_templates/transforms/r_transform_recipe/README.md
@@ -1,13 +1,10 @@
 ## R Transform Template
 
-This is an R transformer template with preproccessing that:
+This is an R transformer template with preproccessing using a recipe that:
 - Handles categorical variables
 - Handles numeric variables
 
 This transformer is intended to work with the [R Drop-In Environment](../../../public_dropin_environments/r_lang/).
-
-Note that transformers differ from custom models in that the end product is not a trained model, but a transformation of the dataset.
-As such, the fit method will train only a preprocessing pipeline on your data.
 
 ## Instructions:
 You can change the contents of the `fit` hook to contain the code you want. The `transform` hook will simply 'bake the recipe'
@@ -30,8 +27,8 @@ This makes a dataset that can be used with any model that supports sparse data.
 
 ### To run locally using 'drum'
 Paths are relative to `datarobot-user-models` root:
-`drum fit --code-dir task_templates/transforms/r_transform --input tests/testdata/iris_binary_training.csv --target-type transform --target Species`
+`drum fit --code-dir task_templates/transforms/r_transform_recipe --input tests/testdata/iris_binary_training.csv --target-type transform --target Species`
 If the command succeeds, your code is ready to be uploaded. 
 
 This template can be run with any downstream problem in mind, so the target could be regression, binary, multiclass, or 
-there could be no target and the preprocessing will treat the full training data as an X matrix.
+there could be no target, making y NULL during fit.
--- task_templates/transforms/r_transform_recipe/custom.R
+++ task_templates/transforms/r_transform_recipe/custom.R
@@ -1,11 +1,10 @@
 library(recipes)
 
-fit <- function(X, y, output_dir, class_order=NULL, row_weights=NULL, ...){
+fit <- function(X, y, output_dir, ...){
   "
   This hook defines how DataRobot will train this task.
   DataRobot runs this hook when the task is being trained inside a blueprint.
-  As an output, this hook is expected to create an artifact containg a trained object, that is then used to transform new data.
-  The input parameters are passed by DataRobot based on project and blueprint configuration.
+  As an output, this hook is expected to create an artifact containing a trained object, that is then used to transform new data.
 
   Parameters
   -------
@@ -15,10 +14,6 @@ fit <- function(X, y, output_dir, class_order=NULL, row_weights=NULL, ...){
       Project's target column.
   output_dir: str
       A path to the output folder; the artifact [in this example - containing the trained recipe] must be saved into this folder.
-  class_order: list
-      [Only passed for a binary estimator] a list containing names of classes: first, the one that is considered negative inside DR, then the one that is considered positive.
-  row_weights: list
-      A list of weights. DataRobot passes it in case of smart downsampling or when weights column is specified in project settings.
 
   Returns
   -------
@@ -37,10 +32,7 @@ fit <- function(X, y, output_dir, class_order=NULL, row_weights=NULL, ...){
     rcp <- recipe(target ~ ., data = train_df)
   }
 
-  outfile <- 'r_transform.rds'
-
-  # set up the preprocessing pipeline
-  rcp <- recipe(target ~ ., data = train_df)
+  # set up the preprocessing recipe
   model_recipe <- rcp %>%
     # Drop constant columns
     step_zv(all_predictors()) %>%
@@ -55,6 +47,7 @@ fit <- function(X, y, output_dir, class_order=NULL, row_weights=NULL, ...){
   model <- prep(model_recipe, train_df)
 
   # Save model
+  outfile <- 'r_transform.rds'
   if(
     substr(output_dir,
           nchar(output_dir),
@@ -76,9 +69,7 @@ fit <- function(X, y, output_dir, class_order=NULL, row_weights=NULL, ...){
 # transform <- function(X, transformer, ...){
 #   "
 #   This hook defines how DataRobot will use the trained object from fit() to transform new data.
-#   DataRobot runs this hook when the task is used for scoring inside a blueprint.
-#   As an output, this hook is expected to return the scored data.
-#   The input parameters are passed by DataRobot based on dataset and blueprint configuration.
+#   As an output, this hook is expected to return the transformed data.
 #
 #   NOTE: If fit() outputs a single recipe artifact, then you do not need to populate the transform() function. It
 #   will automatically call bake as shown below. If your artifact is anything else, you must populate transform().
@@ -90,13 +81,12 @@ fit <- function(X, y, output_dir, class_order=NULL, row_weights=NULL, ...){
 #       Data that DataRobot passes for transforming.
 #   transformer: Any
 #       Trained object, extracted by DataRobot from the artifact created in fit().
-#       In this example, contains the trained recipe extracted from r_transform.rds.
+#       In this example, contains the trained recipe extracted from r_transform_recipe.rds.
 #
 #   Returns
 #   -------
 #   data.frame
-#       Returns a dataframe with scored data
-#       In case of regression, score() must return a dataframe with a single column with column name 'Predictions'.
+#       Returns the transformed dataframe
 #   "
 #   bake(transformer, X)
 # }
