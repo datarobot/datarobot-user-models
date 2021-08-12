2e9c76451d2d8e02715b26e9d1fdac2682e17fe0
Scott Powers <scott.powers@datarobot.com>
2021-08-12 15:50:54 -0400

    don't let R treat the strings as factors.

--- tests/fixtures/r_transform_fit_custom_non_numeric.R
+++ tests/fixtures/r_transform_fit_custom_non_numeric.R
@@ -2,5 +2,5 @@ fit <- function(X, y, output_dir, class_order=NULL, row_weights=NULL, ...){}
 
 transform <- function(X, transformer, y=NULL, ...){
     # ignore the model and output string
-    as.data.frame(sapply(X, as.character))
+    as.data.frame(sapply(X, as.character), stringAsFactors=FALSE)
 }
