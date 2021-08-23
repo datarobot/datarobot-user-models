951a7d8aa19e0d89c3b00b170540bf6bc8ed8a77
Duncan Renfrow-Symon <duncan.renfrow.symon@datarobot.com>
2021-08-23 12:09:21 -0400

    WIP on updating all templates

--- task_templates/pipelines/python3_sklearn_with_custom_classes/custom.py
+++ task_templates/pipelines/python3_sklearn_with_custom_classes/custom.py
@@ -3,10 +3,9 @@
 
     In some cases, avg(prediction) might not match avg(actuals)
     This task, added as a calibrator in the end of a regression blueprint, can help to fix that
-    During fit(), it computes and stores the calibration coefficient that is equal to avg(actuals) / avg(predicted) on training data
+    During fit(), it computes and stores the calibration coefficient that is equal
+    to avg(actuals) / avg(predicted) on training data
     During score(), it multiplies incoming data by the calibration coefficient
-
-
 """
 
 from typing import List, Optional, Any, Dict
@@ -30,48 +29,55 @@ def fit(
 
     This hook defines how DataRobot will train this task.
     DataRobot runs this hook when the task is being trained inside a blueprint.
-    As an output, this hook is expected to create an artifact containg a trained object, that is then used to score new data.
-    The input parameters are passed by DataRobot based on project and blueprint configuration.
+
+    DataRobot will pass the training data, project target, and additional parameters based on the project
+    and blueprint configuration as parameters to this function.
+
+    As an output, this hook is expected to create an artifact containing a trained object,
+    that is then used to score new data.
 
     Parameters
-    -------
-    X
-        Training data that DataRobot passes when this task is being trained.
-    y
+    ----------
+    X: pd.DataFrame
+        Training data that DataRobot passes when this task is being trained. Note that both the training data AND
+        column (feature) names are passed
+    y: pd.Series
         Project's target column.
-    output_dir
+    output_dir: str
         A path to the output folder (also provided in --output paramter of 'drum fit' command)
         The artifact [in this example - containing the trained sklearn pipeline]
         must be saved into this folder.
-    class_order
-        A two element long list dictating the order of classes which should be used for modeling.
+    class_order: Optional[List[str]]
+        This indicates which class DataRobot considers positive or negative. E.g. 'yes' is positive, 'no' is negative.
         Class order will always be passed to fit by DataRobot for classification tasks,
         and never otherwise. When models predict, they output a likelihood of one class, with a
         value from 0 to 1. The likelihood of the other class is 1 - this likelihood. Class order
         dictates that the first element in the list will be the 0 class, and the second will be the
         1 class.
-    row_weights
+    row_weights: Optional[np.ndarray]
         An array of non-negative numeric values which can be used to dictate how important
         a row is. Row weights is only optionally used, and there will be no filtering for which
         custom models support this. There are two situations when values will be passed into
-        row_weights, during smart downsampling and when weights are explicitly provided by the user
+        row_weights, during smart downsampling and when weights are explicitly specified in the project settings.
     kwargs
-        Added for forwards compatibility
+        Added for forwards compatibility.
 
     Returns
     -------
     None
-        fit() doesn't return anything, but must output an artifact (typically containing a trained object) into output_dir
+        fit() doesn't return anything, but must output an artifact
+        (typically containing a trained object) into output_dir
         so that the trained object can be used during scoring.
     """
 
-    # train sklearn pipeline
     estimator = pipeline(X)
     estimator.fit(X, y)
 
-    # dump the trained object [in this example - a trained sklearn pipeline]
+    # Dump the trained object [in this example - a trained XGBoost model]
     # into an artifact [in this example - artifact.pkl]
     # and save it into output_dir so that it can be used later when scoring data
+    # Note: DRUM will automatically load the model when it is in the default format (see docs)
+    # and there is only one artifact file
     output_dir_path = Path(output_dir)
     if output_dir_path.exists() and output_dir_path.is_dir():
         with open("{}/artifact.pkl".format(output_dir), "wb") as fp:
@@ -80,31 +86,32 @@ def fit(
 
 def score(data: pd.DataFrame, model: Any, **kwargs: Dict[str, Any]) -> pd.DataFrame:
     """
+    DataRobot will run this hook when the task is used for scoring inside a blueprint
+
     This hook defines the output of a custom estimator and returns predictions on input data.
     It should be skipped if a task is a transform.
 
     Note: While best practice is to include the score hook, if the score hook is not present DataRobot will
-    add a score hook and call the default predict method for the library, e.g. model.predict(X,y) for python
+    add a score hook and call the default predict method for the library
+    See https://github.com/datarobot/datarobot-user-models#built-in-model-support for details
 
     Parameters
     ----------
-    data:
-        Is the dataframe to make predictions against. If `transform` is supplied,
-        `data` will be the transformed data.
-    model:
+    data: pd.DataFrame
+        Is the dataframe to make predictions against. If the `transform` hook is utilized,
+        `data` will be the transformed data
+    model: Any
         Trained object, extracted by DataRobot from the artifact created in fit().
         In this example, contains trained sklearn pipeline extracted from artifact.pkl.
     kwargs:
         Additional keyword arguments to the method
-        In case of classification model class labels will be provided as the following arguments:
-        - `positive_class_label` is the positive class label for a binary classification model
-        - `negative_class_label` is the negative class label for a binary classification model
 
     Returns
     -------
     This method should return predictions as a dataframe with the following format:
       Binary Classification: must have columns for each class label with floating- point class
-        probabilities as values. Each row should sum to 1.0
+        probabilities as values. Each row should sum to 1.0. The original class names defined in the project
+        must be used as column names.
       Regression: must have a single column called `Predictions` with numerical values
     """
 
