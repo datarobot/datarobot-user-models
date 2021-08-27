2f792b113a22a3293ea4be355f5af25652a4f2fa
Timothy Whittaker <timothy.whittaker@datarobot.com>
2021-08-26 22:29:53 -0400

    add missing value handling to julia model templates

--- model_templates/julia/jl_galaxy/custom.jl
+++ model_templates/julia/jl_galaxy/custom.jl
@@ -5,7 +5,15 @@ using MLJ, MLJScikitLearnInterface, DataFrames
 export MLJ, MLJScikitLearnInterface, transform
 
 function transform(data, model)
-    select!(data, Not(:class))
+    if "Species" in names(data)
+        data = select!(data, Not(:Species))
+    elseif "MEDV" in names(data)
+        data = select!(data, Not(:MEDV))
+    elseif "class" in names(data)
+        data = select!(data, Not(:class))
+    end
+    data = coalesce.(data, 0)
+    return data
 end
 
 end
