c375eb2b1915a53903b691886ddbf342bcf44fe9
Scott Powers <scott.powers@datarobot.com>
2021-09-03 09:20:12 -0400

    add check for Y transform in R

--- custom_model_runner/datarobot_drum/drum/language_predictors/r_predictor/score.R
+++ custom_model_runner/datarobot_drum/drum/language_predictors/r_predictor/score.R
@@ -397,6 +397,9 @@ outer_transform <- function(binary_data=NULL, target_binary_data=NULL, mimetype=
 
         output_data[[1]] <- data.frame(output_data_summary)
         colnames(output_data[[1]]) <- c("__DR__i", "__DR__j", "__DR__x")
+
+    if (!is.null(output_data[[2]]) {
+        stop(sprintf("Transformation of the target variable is not supported by DRUM."))
     }
 
     output_data
