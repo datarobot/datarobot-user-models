562cc69f8d68f93685f72239b4354c0a00ad6834
Scott Powers <scott.powers@datarobot.com>
2021-10-08 11:50:34 -0400

    lbfgs solver has an issue that causes this pipeline to fail sometimes, use liblinear instead

--- task_templates/pipelines/python3_sklearn_multiclass/create_pipeline.py
+++ task_templates/pipelines/python3_sklearn_multiclass/create_pipeline.py
@@ -128,6 +128,6 @@ dense_preprocessing_pipeline = Pipeline(
 
 def make_classifier(X):
     return Pipeline(
-        steps=[("preprocessing", dense_preprocessing_pipeline), ("model", LogisticRegression())],
+        steps=[("preprocessing", dense_preprocessing_pipeline), ("model", LogisticRegression(solver='liblinear'))],
         verbose=True,
     )
