9b34b98da08d869c02cb322c27d5166a52905c3f
Josh Preuss <josh@enjoysailing.us>
2021-10-20 17:42:11 -0400

    init

--- tests/fixtures/fail_classification_validation_custom.R
+++ tests/fixtures/fail_classification_validation_custom.R
@@ -1,3 +1,12 @@
+# Copyright 2021 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# 
+#
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
 # this score rturns predictions which fails a check that probabilities must add up to one
 score <- function(data, model, ...) {
     predictions = data.frame(rbind(c(0.2, 0.7), 1)[rep(1, nrow(data)), ])
--- tests/fixtures/r_lang_hyperparameters/create_pipeline.R
+++ tests/fixtures/r_lang_hyperparameters/create_pipeline.R
@@ -1,3 +1,12 @@
+# Copyright 2021 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# 
+#
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
 create_pipeline<-function(X, y, parameters, model_type='regression') {
   if (is.null(parameters$method)) {
     stop("method parameter value not found")
