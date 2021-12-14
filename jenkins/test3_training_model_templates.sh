6defc0fbc97cbbb7653921feea10ea4f814028da
Yakov Goldberg <yakov.goldberg@datarobot.com>
2021-12-14 05:43:47 -0800

    Release v1.6.2-post1 (#514)

--- jenkins/test3_training_model_templates.sh
+++ jenkins/test3_training_model_templates.sh
@@ -14,10 +14,12 @@ build_drum
 DRUM_WHEEL="$(realpath "$(find custom_model_runner/dist/datarobot_drum*.whl)")"
 build_all_dropin_env_dockerfiles "$DRUM_WHEEL"
 
+# newer version of pip has a more reliable dependency parser
+pip install pip==21.3
 # installing DRUM into the test env is required for push test
-pip install -U $DRUM_WHEEL_REAL_PATH
+python3 -m pip install -U $DRUM_WHEEL_REAL_PATH
 # requirements_test may install newer packages for testing, e.g. `datarobot`
-pip install -r requirements_test.txt
+python3 -m pip install -r requirements_test.txt
 
 # put tests in this exact order as they build images and as a result jenkins instance may run out of space
 py.test tests/functional/test_custom_task_templates.py \
