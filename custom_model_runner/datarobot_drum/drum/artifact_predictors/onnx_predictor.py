8a7f8e5d0aa1597848411508159279c7988bd0ee
Aslı Sabancı <aslisabanci@gmail.com>
2022-01-14 18:49:14 -0800

    Add onnx predictor - to be cont.

--- custom_model_runner/datarobot_drum/drum/artifact_predictors/onnx_predictor.py
+++ custom_model_runner/datarobot_drum/drum/artifact_predictors/onnx_predictor.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021 DataRobot, Inc. and its affiliates.
+Copyright 2022 DataRobot, Inc. and its affiliates.
 All rights reserved.
 This is proprietary source code of DataRobot, Inc. and its affiliates.
 Released under the terms of DataRobot Tool and Utility Agreement.
@@ -9,62 +9,67 @@ from datarobot_drum.drum.artifact_predictors.artifact_predictor import ArtifactP
 from datarobot_drum.drum.enum import extra_deps, PythonArtifacts, SupportedFrameworks
 
 
-class KerasPredictor(ArtifactPredictor):
+class ONNXPredictor(ArtifactPredictor):
     def __init__(self):
-        super(KerasPredictor, self).__init__(
-            SupportedFrameworks.KERAS, PythonArtifacts.KERAS_EXTENSION
+        super(ONNXPredictor, self).__init__(
+            SupportedFrameworks.ONNX, PythonArtifacts.ONNX_EXTENSION
         )
         self._model = None
 
     def is_framework_present(self):
         try:
-            from tensorflow.keras.models import load_model
+            #TODO: Asli: Any better checks?
+            from onnxruntime import onnxruntime_validation
 
             return True
         except ImportError:
             return False
 
     def framework_requirements(self):
-        return extra_deps[SupportedFrameworks.KERAS]
+        return extra_deps[SupportedFrameworks.ONNX]
 
     def can_load_artifact(self, artifact_path):
         if not self.is_artifact_supported(artifact_path):
             return False
 
         try:
-            from tensorflow.keras.models import load_model
+            from onnxruntime import onnxruntime_validation
 
             return True
         except ImportError:
             return False
 
     def can_use_model(self, model):
-        if not self.is_framework_present():
-            return False
-
-        try:
-            from sklearn.pipeline import Pipeline
-            from tensorflow import keras as keras_tf
-
-            if isinstance(model, Pipeline):
-                # check the final estimator in the pipeline is Keras
-                if isinstance(model[-1], keras_tf.Model):
-                    return True
-            elif isinstance(model, keras_tf.Model):
-                return True
-        except Exception as e:
-            self._logger.debug("Exception: {}".format(e))
-            return False
-        return False
+        #TODO:Asli - implement
+        return True
+        # if not self.is_framework_present():
+        #     return False
+        #
+        # try:
+        #     from sklearn.pipeline import Pipeline
+        #     from tensorflow import keras as keras_tf
+        #
+        #     if isinstance(model, Pipeline):
+        #         # check the final estimator in the pipeline is Keras
+        #         if isinstance(model[-1], keras_tf.Model):
+        #             return True
+        #     elif isinstance(model, keras_tf.Model):
+        #         return True
+        # except Exception as e:
+        #     self._logger.debug("Exception: {}".format(e))
+        #     return False
+        # return False
 
     def load_model_from_artifact(self, artifact_path):
-        from tensorflow.keras.models import load_model
+        import onnxruntime as ort
 
-        self._model = load_model(artifact_path, compile=False)
+        self._model = ort.InferenceSession(artifact_path)
         return self._model
 
     def predict(self, data, model, **kwargs):
-        super(KerasPredictor, self).predict(data, model, **kwargs)
-        predictions = model.predict(data)
+        super(ONNXPredictor, self).predict(data, model, **kwargs)
 
+        input_name = [input.name for input in self._model.get_inputs()]
+        out_name = [output.name for output in self._model.get_outputs()]
+        predictions = self._model.run(out_name, {input_name[0]: data})
         return predictions, None
