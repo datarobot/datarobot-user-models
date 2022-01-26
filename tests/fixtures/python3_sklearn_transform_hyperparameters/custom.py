df6fe0b10e0bf5ba0bb354bb5656a23566dc7f48
Jesse Bannon <jesse.bannon@datarobot.com>
2022-01-26 12:38:43 -0800

    black

--- tests/fixtures/python3_sklearn_transform_hyperparameters/custom.py
+++ tests/fixtures/python3_sklearn_transform_hyperparameters/custom.py
@@ -12,7 +12,6 @@ from sklearn.preprocessing import KBinsDiscretizer
 
 
 class CustomTask(TransformerInterface):
-
     def fit(self, X, y, parameters=None, **kwargs):
         """ This numeric transform example showcases how to define and use hyperparameters. Hyperparameters are defined
         in model-metadata.yaml, and are passed into fit as a dict.
@@ -55,28 +54,27 @@ class CustomTask(TransformerInterface):
         # Parameters should always be passed in as a dict if hyperparameters are defined in the model-metadata.
         if parameters is None:
             raise ValueError(
-                'Parameters were not passed into the custom task. '
-                'Ensure your model metadata contains hyperparameter definitions'
+                "Parameters were not passed into the custom task. "
+                "Ensure your model metadata contains hyperparameter definitions"
             )
 
         # A useless parameter show-casing you can pass arbitrary strings
-        print(parameters['print_message'])
+        print(parameters["print_message"])
 
         # Set the seed for reproducible fit behavior.
-        np.random.seed(parameters['seed'])
+        np.random.seed(parameters["seed"])
 
         # First fit the SimpleImputer. Check the missing_values parameter to see if it's a numeric, if so, use the
         # 'constant' strategy and set the fill value to the numeric.
         missing_values_fill_value = None
-        if isinstance(parameters['missing_values_strategy'], (int, float)):
-            missing_values_strategy = 'constant'
-            missing_values_fill_value = parameters['missing_values_strategy']
+        if isinstance(parameters["missing_values_strategy"], (int, float)):
+            missing_values_strategy = "constant"
+            missing_values_fill_value = parameters["missing_values_strategy"]
         else:
-            missing_values_strategy = parameters['missing_values_strategy']
+            missing_values_strategy = parameters["missing_values_strategy"]
 
         self.missing_vals_transformer = SimpleImputer(
-            strategy=missing_values_strategy,
-            fill_value=missing_values_fill_value,
+            strategy=missing_values_strategy, fill_value=missing_values_fill_value,
         ).fit(X, y)
 
         # Then, we need to transform the data to use as training data for the second transformer.
@@ -84,9 +82,9 @@ class CustomTask(TransformerInterface):
 
         # Now we fit the second transformer, and use hyperparameters to choose the number of bins and strategy.
         self.kbins_transformer = KBinsDiscretizer(
-            n_bins=parameters['kbins_n_bins'],
-            strategy=parameters['kbins_strategy'],
-            encode='onehot',
+            n_bins=parameters["kbins_n_bins"],
+            strategy=parameters["kbins_strategy"],
+            encode="onehot",
         )
         self.kbins_transformer.fit(X, y)
 
