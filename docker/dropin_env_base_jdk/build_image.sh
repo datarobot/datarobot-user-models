9bda58b71a41e7434a901b222a8039768d53e840
Yakov Goldberg <yakov.goldberg@datarobot.com>
2022-03-22 08:02:55 -0700

    address review comments

--- docker/dropin_env_base_jdk/build_image.sh
+++ docker/dropin_env_base_jdk/build_image.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copyright 2021 DataRobot, Inc. and its affiliates.
 #
 # All rights reserved.
@@ -6,8 +6,7 @@
 #
 # Released under the terms of DataRobot Tool and Utility Agreement.
 
-GIT_ROOT=$(git rev-parse --show-toplevel)
-TEST_DOCKER_DIR=$GIT_ROOT/docker/drum_integration_tests_base
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 IMAGE_NAME=datarobot/dropin-env-base
 IMAGE_TAG=debian10-py3.7-jdk11-drum1.8.0-mlops8.0.7
@@ -15,5 +14,5 @@ IMAGE_TAG=debian10-py3.7-jdk11-drum1.8.0-mlops8.0.7
 pwd
 
 echo "Building docker image: ${IMAGE_NAME}:${IMAGE_TAG}"
-DATAROBOT_MLOPS_VERSION=8.0.7 ./pull_artifacts.sh
+DATAROBOT_MLOPS_VERSION=8.0.7 ${SCRIPT_DIR}/pull_artifacts.sh
 docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
