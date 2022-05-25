9c33096297b6db0c8aa367fba40644d7513c6dbe
Scott Powers <scott.powers@datarobot.com>
2022-05-25 09:04:05 -0400

    demo task using large huggingface model

---
+++ task_templates/2_estimators/python-huggingface-vit/custom.py
@@ -0,0 +1,121 @@
+import sys
+from pathlib import Path
+
+from transformers import AutoFeatureExtractor, AutoModelForImageClassification, Trainer, TrainingArguments
+
+
+from typing import List, Optional, Any, Dict
+import pandas as pd
+import numpy as np
+import torch
+from img_utils import b64_to_img
+from datarobot_drum.custom_task_interfaces import BinaryEstimatorInterface
+
+
+class DataSet(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+
+class CustomTask(BinaryEstimatorInterface):
+    def fit(self, X, y, row_weights=None, **kwargs):
+        self.class_order_to_lookup(kwargs['class_order'])
+        # load original model
+        self.extractor = AutoFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
+        estimator = AutoModelForImageClassification.from_pretrained("google/vit-base-patch16-224",
+                                                                         num_labels=len(kwargs['class_order']),
+                                                                         id2label=self.id2label,
+                                                                         label2id=self.label2id,
+                                                                         ignore_mismatched_sizes=True,)
+        # Create a training dataset with pytorch
+        train_encoded = self.extractor(images=X.iloc[:, 0].apply(b64_to_img).values.tolist(), return_tensors="pt")
+        train_dataset = DataSet(train_encoded, y.map(lambda v: int(self.label2id[v])))
+
+        training_args = TrainingArguments(
+            output_dir=kwargs['output_dir']+'/training_tmp',
+            num_train_epochs=3,
+            no_cuda=True,
+            #do_train=True,
+        )
+        trainer = Trainer(
+            model=estimator,
+            args=training_args,
+            train_dataset=train_dataset,
+        )
+        trainer.train()
+        self.estimator=trainer
+
+    def class_order_to_lookup(self, class_order):
+        self.label2id, self.id2label = dict(), dict()
+        for i, label in enumerate(class_order):
+            self.label2id[label] = str(i)
+            self.id2label[str(i)] = label
+
+    def save(self, artifact_directory):
+        """
+        Serializes the object and stores it in `artifact_directory`
+
+        Parameters
+        ----------
+        artifact_directory: str
+            Path to the directory to save the serialized artifact(s) to.
+
+        Returns
+        -------
+        self
+        """
+
+        # If your estimator is not pickle-able, you can serialize it using its native method,
+        # i.e. in this case for pytorch we use model.save, and then set the estimator to none
+        self.estimator.save_model(Path(artifact_directory)/'vit')
+        # # Helper method to handle serializing, via pickle, the CustomTask class
+        self.save_task(artifact_directory, exclude=["estimator"])
+
+        return self
+
+    @classmethod
+    def load(cls, artifact_directory):
+        """
+        Deserializes the object stored within `artifact_directory`
+
+        Returns
+        -------
+        cls
+            The deserialized object
+        """
+        custom_task = cls.load_task(artifact_directory)
+        custom_task.estimator = AutoModelForImageClassification.from_pretrained(Path(artifact_directory)/'vit'
+                                                                                )
+
+        custom_task.extractor = AutoFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
+
+        return custom_task
+
+    def predict_proba(self, X, **kwargs):
+        print('predicting')
+        assert len(X.columns) == 1, "This only works with a single image column"
+        images = X.iloc[:, 0].apply(b64_to_img).values.tolist()
+        batch_size = 5
+        size = len(images)
+        preds = None
+        for idx in range(0, size, batch_size):
+            img_slice = images[idx:idx+batch_size]
+            inputs = self.extractor(images=img_slice, return_tensors="pt")
+            outputs = self.estimator(**inputs)
+            predictions = torch.nn.functional.softmax(outputs.logits, dim=-1).detach().numpy()
+            print(predictions.shape)
+            if preds is None:
+                preds = predictions
+            else:
+                preds = np.vstack((preds, predictions))
+        df = pd.DataFrame(preds, columns=self.label2id.keys())
+        return df
