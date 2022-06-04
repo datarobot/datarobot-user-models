d45455113584a147dcd55f7848033d91ffe69eed
Yakov Goldberg <yakov.goldberg@datarobot.com>
2022-06-03 22:43:12 -0700

    [POC] Python orchestrator running multiple scoring code predictors

---
+++ model_templates/python_multiple_codegen/README.md
@@ -0,0 +1,16 @@
+## Python + Multiple CodeGen models
+
+### Build java environment
+`cd ./datarobot-user-models/public_dropin_environments/java_codegen`   
+`docker build -t java_env .`
+
+Then run locally
+### To run locally using 'drum'
+Paths are relative to `./datarobot-user-models`:   
+
+`drum score --code-dir model_templates/python_multiple_codegen/ --target-type regression --input tests/testdata/juniors_3_year_stats_regression.csv --docker java_env`
+
+### To run DRUM as a server
+`drum server --code-dir model_templates/python_multiple_codegen/ --target-type regression  --docker java_env --address 0.0.0.0:4567`
+To make predictions:
+`curl -X POST --form "X=@./tests/testdata/juniors_3_year_stats_regression.csv" 0.0.0.0:4567/predict/`
---
+++ model_templates/python_multiple_codegen/custom.py
@@ -0,0 +1,37 @@
+"""
+Copyright 2021 DataRobot, Inc. and its affiliates.
+All rights reserved.
+This is proprietary source code of DataRobot, Inc. and its affiliates.
+Released under the terms of DataRobot Tool and Utility Agreement.
+"""
+from datarobot_drum.drum.language_predictors.java_predictor.java_predictor import JavaPredictor
+
+
+class ScoringCodePredictor(JavaPredictor):
+    def __init__(self, code_dir, target_type):
+        super(ScoringCodePredictor, self).__init__()
+
+        params = {"__custom_model_path__": code_dir, "target_type": target_type}
+
+        self.mlpiper_configure(params)
+
+    def predict(self, df):
+        s = df.to_csv(index=False)
+        bin_data = s.encode()
+        return super(ScoringCodePredictor, self).predict(binary_data=bin_data)
+
+
+def load_model(code_dir):
+    predictor1 = ScoringCodePredictor("{}/junior_reg_1".format(code_dir), "regression")
+    predictor2 = ScoringCodePredictor("{}/junior_reg_2".format(code_dir), "regression")
+    return predictor1, predictor2
+
+
+def score(data, model, **kwargs):
+    pred_df1 = model[0].predict(data)
+    pred_df2 = model[1].predict(data)
+
+    if pred_df1.iloc[0]["Predictions"] > pred_df2.iloc[0]["Predictions"]:
+        return pred_df1
+    else:
+        return pred_df2
