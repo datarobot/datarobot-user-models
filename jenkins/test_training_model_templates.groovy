9f9a62e96ad2e148686a0c0404ac68b81c3a8c05
Yakov Goldberg <yakov.goldberg@datarobot.com>
2022-06-29 10:07:32 -0700

    fix many things

--- jenkins/test_training_model_templates.groovy
+++ jenkins/test_training_model_templates.groovy
@@ -1,29 +1,32 @@
-node('cpu-intense && release-dev'){
+node('release-dev && memory-intense'){
   checkout scm
-  stage ('Checkout') {
-      checkout scm
-  }
-  dir('jenkins_artifacts'){
-      unstash 'drum_wheel'
-  }
+  stage ('test_training_model_templates') {
+    checkout scm
 
-  withQuantum([
-    bash: '''\
-        set -exuo pipefail
-        ls -la jenkins_artifacts
-        pushd ./../DataRobot
-        make update_env
-        export LRS_POLL_DELAY_SECONDS=1
-        export LRS_RETRIES=120
-        export CUSTOM_MODEL_PREDICT_MEM_LIMIT=2147483648
-        export CUSTOM_MODEL_PREDICT_MEM_REQUEST=2147483648
-        export EXECUTION_ENVIRONMENT_LIMIT=20
-        export RESTRICT_EXECUTION_ENVIRONMENT_CREATION=false
-        export CLIENT_VERSION=\$(git rev-parse --short HEAD)
-        ./start.sh --kubernetes-k3d --kubernetes-validate
-        popd
-        ./jenkins/test3_training_model_templates.sh
-    '''.stripIndent(),
-    pythonVersion: '3.7',
-  ])
+    dir('jenkins_artifacts'){
+        unstash 'drum_wheel'
+    }
+
+    checkoutDataRobot()
+    withQuantum([
+        bash: '''\
+            set -exuo pipefail
+            ls -la jenkins_artifacts
+            pushd DataRobot
+            make update_env
+            export LRS_POLL_DELAY_SECONDS=1
+            export LRS_RETRIES=120
+            export CUSTOM_MODEL_PREDICT_MEM_LIMIT=2147483648
+            export CUSTOM_MODEL_PREDICT_MEM_REQUEST=2147483648
+            export EXECUTION_ENVIRONMENT_LIMIT=20
+            export RESTRICT_EXECUTION_ENVIRONMENT_CREATION=false
+            export CLIENT_VERSION=\$(git rev-parse --short HEAD)
+            ./start.sh --kubernetes-k3d --kubernetes-validate
+            popd
+            ./jenkins/test3_training_model_templates.sh
+        '''.stripIndent(),
+        pythonVersion: '3',
+        venvName: "datarobot-dev"
+    ])
+  }
 }
