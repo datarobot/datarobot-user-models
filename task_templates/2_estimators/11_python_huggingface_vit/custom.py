20265238b4417930cb7dbfbbecf184fb5cfb01f9
Scott Powers <54544537+scottp-ml@users.noreply.github.com>
2022-06-30 20:31:44 -0400

    [MODEL-9532] faster vit (#656)

--- task_templates/2_estimators/11_python_huggingface_vit/custom.py
+++ task_templates/2_estimators/11_python_huggingface_vit/custom.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from transformers import (
@@ -36,7 +35,7 @@ class CustomTask(BinaryEstimatorInterface):
         self.pretrained_location = Path(__file__).resolve().parent / self.model_name
         self.class_order_to_lookup(kwargs["class_order"])
         # load base transformer featurizer and model
-        self.extractor = AutoFeatureExtractor.from_pretrained(self.pretrained_location)
+        extractor = AutoFeatureExtractor.from_pretrained(self.pretrained_location)
         estimator = AutoModelForImageClassification.from_pretrained(
             self.pretrained_location,
             num_labels=len(kwargs["class_order"]),
@@ -47,16 +46,20 @@ class CustomTask(BinaryEstimatorInterface):
 
         # Create a training dataset with pytorch
         # Images are in b64 encoded format, and have to be turned into Image objects and then encoded using the encoder
-        train_encoded = self.extractor(
+        train_encoded = extractor(
             images=X.iloc[:, 0].apply(b64_to_img).values.tolist(), return_tensors="pt"
         )
         train_dataset = DataSet(train_encoded, y.map(lambda v: int(self.label2id[str(v)])))
 
         # Setup training arguments and the Huggingface trainer to facilitate fine tuning
         training_args = TrainingArguments(
-            output_dir=kwargs["output_dir"] + "/training_tmp", num_train_epochs=3, no_cuda=True,
+            output_dir=kwargs["output_dir"] + "/training_tmp",
+            num_train_epochs=3,
+            no_cuda=True,
+            jit_mode_eval=True,
+            use_ipex=True,
         )
-        trainer = Trainer(model=estimator, args=training_args, train_dataset=train_dataset,)
+        trainer = Trainer(model=estimator, args=training_args, train_dataset=train_dataset)
         trainer.train()
         self.estimator = trainer
 
