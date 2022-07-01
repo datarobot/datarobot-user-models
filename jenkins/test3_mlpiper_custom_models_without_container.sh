ad3b30080f5064e556dfbdc03c71494817e49f92
Yakov Goldberg <yakov.goldberg@datarobot.com>
2022-06-30 17:47:53 -0700

    cleanup

--- jenkins/test3_mlpiper_custom_models_without_container.sh
+++ jenkins/test3_mlpiper_custom_models_without_container.sh
@@ -47,7 +47,7 @@ mvn package
 popd
 
 
-# only test here tests which were sequential historically
+# only run here tests which were sequential historically
 
 pytest tests/drum/test_inference_custom_java_predictor.py tests/drum/test_mlops_monitoring.py \
        -m "sequential" \
--- tests/drum/run-drum-tests-in-container.sh
+++ tests/drum/run-drum-tests-in-container.sh
@@ -59,13 +59,6 @@ source "${GIT_ROOT}/tools/image-build-utils.sh"
 echo "--> Change every environment Dockerfile to install local freshly built DRUM wheel: ${DRUM_WHEEL_REAL_PATH}"
 build_all_dropin_env_dockerfiles "${DRUM_WHEEL_REAL_PATH}"
 
-echo
-echo "--> Compiling jar for TestCustomPredictor "
-echo
-pushd $GIT_ROOT/tests/drum/custom_java_predictor
-mvn package
-popd
-
 source $GIT_ROOT/tests/drum/integration-helpers.sh
 
 cd $GIT_ROOT || exit 1
