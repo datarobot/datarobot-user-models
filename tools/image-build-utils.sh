5a0941e833521fadef23951bc473f122d2e7d84e
Yakov Goldberg <yakov.goldberg@datarobot.com>
2022-08-22 12:07:03 +0300

    install uwsgin into every env during the tests

--- tools/image-build-utils.sh
+++ tools/image-build-utils.sh
@@ -44,6 +44,9 @@ function build_dropin_env_dockerfile() {
   sed -i "/COPY \+dr_requirements.txt \+dr_requirements.txt/a COPY ${DRUM_WHEEL_FILENAME} ${DRUM_WHEEL_FILENAME}" Dockerfile
   # replace 'datarobot-drum' requirement with a wheel
   sed -i "s/^datarobot-drum.*/${DRUM_WHEEL_FILENAME}${WITH_R}/" dr_requirements.txt
+  # In general we want DRUM not to depend on uwsgi, so install it explicitly for the tests
+  echo "uwsgi" >> dr_requirements.txt
+
   popd || exit 1
 }
 
