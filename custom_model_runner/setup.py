ef08d36c64a3d7bdd41a619b07a72f45d0540b0f
Yakov Goldberg <yakov.goldberg@datarobot.com>
2022-09-29 12:43:40 -0700

    [RAPTOR-8618] pin dr==2.28.1

--- custom_model_runner/setup.py
+++ custom_model_runner/setup.py
@@ -26,7 +26,6 @@ extras_require = {
     "xgboost": extra_deps[SupportedFrameworks.XGBOOST],
     "R": ["rpy2==3.5.2;python_version>='3.6'"],
     "pypmml": extra_deps[SupportedFrameworks.PYPMML],
-    "trainingModels": ["datarobot==2.27.0"],
     "uwsgi": ["uwsgi"],
 }
 
