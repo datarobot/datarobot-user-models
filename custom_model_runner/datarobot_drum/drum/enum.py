71284f8419b8f3cbdea0cd97e403b92d7b1d690e
Erik Lattimore <elatt@users.noreply.github.com>
2023-01-30 17:30:19 -0500

    Allow runtime params in metadata file
    We don't need to define the schema in DRUM since the MLOps platform will
    pass all runtime params via env vars.

--- custom_model_runner/datarobot_drum/drum/enum.py
+++ custom_model_runner/datarobot_drum/drum/enum.py
@@ -375,6 +375,9 @@ class ModelMetadataKeys(object):
     # customPredictor section is not used by DRUM,
     # it is a place holder if user wants to add some fields and read them on his own
     CUSTOM_PREDICTOR = "customPredictor"
+    # runtimeParameters section is not used by DRUM; it is used by the MLOps platform
+    # so can be present in the file but should be ignored.
+    RUNTIME_PARAMETERS = "runtimeParameters"
 
 
 class ModelMetadataHyperParamTypes(object):
