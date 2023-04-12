8fa779f7b89a8eced2b736420feac4c1993ab47c
Eric Shaw <eric.shaw@datarobot.com>
2023-04-12 16:42:20 -0400

    added docstrings

--- custom_model_runner/datarobot_drum/custom_task_interfaces/estimator_interfaces.py
+++ custom_model_runner/datarobot_drum/custom_task_interfaces/estimator_interfaces.py
@@ -26,9 +26,28 @@ class EstimatorInterface(CustomTaskInterface):
 
 class BinaryEstimatorInterface(EstimatorInterface):
     def predict(self, X, **kwargs):
+        """Binary and Multiclass ignore the `predict` method and must implement the `predict_proba` method."""
         pass
 
     def predict_proba(self, X, **kwargs):
+        """
+        This hook defines how DataRobot will use the trained object from fit() to predict new data.
+        DataRobot runs this hook when the task is used for scoring inside a blueprint.
+        As an output, this hook is expected to return the predicted data.
+        The input parameters are passed by DataRobot based on dataset and blueprint configuration.
+
+        Parameters
+        -------
+        X: pd.DataFrame
+            Data that DataRobot passes for scoring.
+
+        Returns
+        -------
+        pd.DataFrame
+            Returns a dataframe with predict data.
+            In case of regression, predict() must return a dataframe with a single column with column
+            name "Predictions".
+        """
         raise NotImplementedError()
 
 
@@ -38,9 +57,28 @@ class RegressionEstimatorInterface(EstimatorInterface):
 
 class MulticlassEstimatorInterface(EstimatorInterface):
     def predict(self, X, **kwargs):
+        """Binary and Multiclass ignore the `predict` method and must implement the `predict_proba` method."""
         pass
 
     def predict_proba(self, X, **kwargs):
+        """
+        This hook defines how DataRobot will use the trained object from fit() to predict new data.
+        DataRobot runs this hook when the task is used for scoring inside a blueprint.
+        As an output, this hook is expected to return the predicted data.
+        The input parameters are passed by DataRobot based on dataset and blueprint configuration.
+
+        Parameters
+        -------
+        X: pd.DataFrame
+            Data that DataRobot passes for scoring.
+
+        Returns
+        -------
+        pd.DataFrame
+            Returns a dataframe with predict data.
+            In case of regression, predict() must return a dataframe with a single column with column
+            name "Predictions".
+        """
         raise NotImplementedError()
 
 
