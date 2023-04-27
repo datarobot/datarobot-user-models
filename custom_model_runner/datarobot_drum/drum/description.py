d91cd2a787b1ebd3024d46c54ca4e700848908bc
Scott Powers <scott.powers@datarobot.com>
2023-04-27 09:02:04 -0400

    dev1 test

--- custom_model_runner/datarobot_drum/drum/description.py
+++ custom_model_runner/datarobot_drum/drum/description.py
@@ -4,6 +4,6 @@ All rights reserved.
 This is proprietary source code of DataRobot, Inc. and its affiliates.
 Released under the terms of DataRobot Tool and Utility Agreement.
 """
-version = "1.10.2"
+version = "1.10.2dev1"
 __version__ = version
 project_name = "datarobot-drum"
