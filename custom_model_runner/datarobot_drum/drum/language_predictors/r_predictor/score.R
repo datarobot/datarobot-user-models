81c8148d299235b1bd0b5e49625ca12331a2bf58
Scott Powers <scott.powers@datarobot.com>
2023-05-02 10:20:47 -0400

    dev3 test

--- custom_model_runner/datarobot_drum/drum/language_predictors/r_predictor/score.R
+++ custom_model_runner/datarobot_drum/drum/language_predictors/r_predictor/score.R
@@ -130,9 +130,10 @@ load_serialized_model <- function(model_dir, target_type) {
         }
     } else {
         tmp <- stri_conv(binary_data, "utf8")
-        data <- read.csv(text=gsub("\r","", tmp, fixed=TRUE), check.names = FALSE)
-        if (ncol(data) == 1 {
-            data <- read.csv(text=gsub("\r","", tmp, fixed=TRUE), check.names = FALSE, blank.lines.skip =FALSE)
+        text <- gsub("\r","", tmp, fixed=TRUE)
+        data <- read.csv(text=text, check.names = FALSE)
+        if (ncol(data) == 1) {
+            data <- read.csv(text=text, check.names = FALSE, blank.lines.skip =FALSE)
         }
     }
     sprintf(dim(data))
