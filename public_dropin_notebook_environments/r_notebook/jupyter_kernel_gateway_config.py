9985cedd57d9eec57efad1d807f87c27664f5c3c
Volodymyr Onofriychuk <volodymyr.onofriichuk@datarobot.com>
2023-05-18 16:44:21 +0300

    Bump notebooks dropins

--- public_dropin_notebook_environments/r_notebook/jupyter_kernel_gateway_config.py
+++ public_dropin_notebook_environments/r_notebook/jupyter_kernel_gateway_config.py
@@ -5,9 +5,22 @@
 # and its affiliates.
 # The copyright notice above does not evidence any actual or intended
 # publication of such source code.
-c.KernelGatewayApp.ip = "0.0.0.0"  # nosec
+c.KernelGatewayApp.ip = '0.0.0.0'  # nosec
 c.KernelGatewayApp.prespawn_count = 1
 c.KernelGatewayApp.max_kernels = 1
-c.KernelGatewayApp.default_kernel_name = "ir"
+c.KernelGatewayApp.default_kernel_name = 'ir'
 c.JupyterWebsocketPersonality.list_kernels = True
 c.KernelRestarter.restart_limit = 3
+
+c.KernelGatewayApp.logging_config = {
+    "formatters": {
+        "console": {
+            "class": "ecs_logging.StdlibFormatter"
+        },
+    },
+    "loggers": {
+        "KernelGatewayApp": {
+            "handlers": ["console"],
+        }
+    },
+}
