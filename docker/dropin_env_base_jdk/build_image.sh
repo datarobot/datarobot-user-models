039f26d77acda89b0ec96cec7a4deec04f4d8147
Yakov Goldberg <yakov.goldberg@datarobot.com>
2023-05-23 10:48:58 -0700

    [RAPTOR-9390] bump env base images (#767)

--- docker/dropin_env_base_jdk/build_image.sh
+++ docker/dropin_env_base_jdk/build_image.sh
@@ -9,10 +9,15 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 IMAGE_NAME=datarobot/dropin-env-base-jdk
-IMAGE_TAG=debian11-py3.9-jdk11.0.16-drum1.9.10-mlops8.2.7
+IMAGE_TAG=debian11-py3.9-jdk11.0.16-drum1.10.3-mlops9.0.7
 
 pwd
 
 echo "Building docker image: ${IMAGE_NAME}:${IMAGE_TAG}"
-DATAROBOT_MLOPS_VERSION=8.2.7 ${SCRIPT_DIR}/pull_artifacts.sh
-docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
+DATAROBOT_MLOPS_VERSION=9.0.7 ${SCRIPT_DIR}/pull_artifacts.sh
+
+# this is just a regular command to build an image for the host platform
+#docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
+
+# this is command to build images for specified platforms. For more info: https://docs.docker.com/build/building/multi-platform/
+docker buildx build --build-arg DATAROBOT_MLOPS_VERSION=${DATAROBOT_MLOPS_VERSION} --platform linux/amd64,linux/arm64 --push -t ${IMAGE_NAME}:${IMAGE_TAG} .
