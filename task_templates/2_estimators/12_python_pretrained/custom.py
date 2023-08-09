6962a5063ffa6d1ce2a0d7ba37483d7619411f91
Scott Powers <54544537+scottp-ml@users.noreply.github.com>
2023-08-09 12:08:01 -0400

    update copyright
    Co-authored-by: Alex Shoop <alexander.shoop@datarobot.com>

--- task_templates/2_estimators/12_python_pretrained/custom.py
+++ task_templates/2_estimators/12_python_pretrained/custom.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021 DataRobot, Inc. and its affiliates.
+Copyright 2023 DataRobot, Inc. and its affiliates.
 All rights reserved.
 This is proprietary source code of DataRobot, Inc. and its affiliates.
 Released under the terms of DataRobot Tool and Utility Agreement.
