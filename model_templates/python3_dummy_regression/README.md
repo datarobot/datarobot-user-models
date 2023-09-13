02ad67f02e30e3dac9f11e1cc3cdf3c2be85816d
Bogdan Klichuk <bogdan.klichuk@datarobot.com>
2023-09-13 22:49:20 +0300

    Add regression dummy custom model

---
+++ model_templates/python3_dummy_regression/README.md
@@ -0,0 +1,13 @@
+## Python Dummy Regression Inference Model Template
+
+This regression model is a very simply example that always yields fixed values. I works with any Python environment that has `pandas`.
+Any target can be set for this model.
+
+## Instructions
+Create a new custom model with this `custom.py` and use any Python Drop-In Environment with it.
+
+### To run locally using 'drum'
+Paths are relative to `./datarobot-user-models`:
+`drum score --code-dir model_templates/python3_dummy_regression --target-type regression --input tests/testdata/juniors_3_year_stats_regression.csv`
+
+Note: any input dataset will work for this model.
---
+++ model_templates/python3_dummy_regression/custom.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from typing import Any, Dict
+
+import pandas as pd
+
+
+def load_model(code_dir: str) -> Any:
+    """
+    Can be used to load supported models if your model has multiple artifacts, or for loading
+    models that **drum** does not natively support
+
+    Parameters
+    ----------
+    code_dir : is the directory where model artifact and additional code are provided, passed in
+
+    Returns
+    -------
+    If used, this hook must return a non-None value
+    """
+    return 'dummy'
+
+
+def score(data: pd.DataFrame, model: Any, **kwargs: Dict[str, Any]) -> pd.DataFrame:
+    """
+    This hook is only needed if you would like to use **drum** with a framework not natively
+    supported by the tool.
+
+    Parameters
+    ----------
+    data : is the dataframe to make predictions against. If `transform` is supplied,
+    `data` will be the transformed data.
+    model : is the deserialized model loaded by **drum** or by `load_model`, if supplied
+    kwargs : additional keyword arguments to the method
+
+    Returns
+    -------
+    This method should return predictions as a dataframe with the following format:
+      Regression: must have a single column called `Predictions` with numerical values
+    """
+    preds = pd.DataFrame([42 for _ in range(data.shape[0])], columns=['Predictions'])
+    return preds
