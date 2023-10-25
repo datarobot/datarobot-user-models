1ae82a7c4a2fa61da32719d875fe28438345f941
Eric Shaw <eric.shaw@datarobot.com>
2023-10-25 09:52:27 -0400

    [MODEL-12503] update test suites (#839)
    * first pass
    
    * update controller
    
    * updated
    
    * added requirments file
    
    * changed requirements file name
    
    * forget a route
    
    * updated test result
    
    * oops

---
+++ jenkins/test_unit.groovy
@@ -0,0 +1,21 @@
+node('multi-executor && ubuntu:focal'){
+    checkout scm
+
+    docker.image('python:3.8').inside() {
+        stage('test_unit') {
+            try {
+                sh"""#!/bin/bash
+                set -exuo pipefail
+                python -m venv /tmp/venv
+                . /tmp/venv/bin/activate
+                pip install -U pip
+                pip install -r requirements_test_unit.txt
+                pip install -e custom_model_runner/
+                pytest tests/unit --junit-xml="unit-test-report.xml"
+                """
+            } finally {
+              junit allowEmptyResults: true, testResults: '**/unit-test-report.xml'
+            }
+        }
+    }
+}
