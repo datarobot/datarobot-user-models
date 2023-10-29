9651e2729ca8cb953de4457760cc61f49df4a243
Zohar Mizrahi <zohar.mizrahi@datarobot.com>
2023-10-29 16:54:17 +0200

    [RAPTOR-10016] DRUM: fix a potential flaky unit test concerning the stdout flusher

--- tests/unit/datarobot_drum/resource/components/Python/prediction_server/test_stdout_flusher.py
+++ tests/unit/datarobot_drum/resource/components/Python/prediction_server/test_stdout_flusher.py
@@ -1,4 +1,3 @@
-import time
 from unittest.mock import patch
 
 from datarobot_drum.resource.components.Python.prediction_server.stdout_flusher import StdoutFlusher
@@ -6,20 +5,6 @@ from datarobot_drum.resource.components.Python.prediction_server.stdout_flusher 
 
 class TestStdoutFlusher:
     """Contains cases to test the StdoutFlusher functionality."""
-
-    def test_start_and_stop(self):
-        """Test the life-cycle methods and validate liveliness."""
-
-        stdout_flusher = StdoutFlusher()
-        assert not stdout_flusher.is_alive()
-        stdout_flusher.stop()
-        assert not stdout_flusher.is_alive()
-        stdout_flusher.start()
-        try:
-            assert stdout_flusher.is_alive()
-        finally:
-            stdout_flusher.stop()
-            assert not stdout_flusher.is_alive()
 
     def test_no_prediction_activity(self):
         """
@@ -32,13 +17,9 @@ class TestStdoutFlusher:
             StdoutFlusher, "_is_predict_time_set_and_max_waiting_time_expired", return_value=False
         ) as condition_method, patch.object(StdoutFlusher, "_flush_stdout") as flush_stdout:
             stdout_flusher = StdoutFlusher(max_time_until_flushing=max_time_until_flushing)
-            stdout_flusher.start()
-            try:
-                time.sleep(max_time_until_flushing * 2)
-                condition_method.assert_called()
-                flush_stdout.assert_not_called()
-            finally:
-                stdout_flusher.stop()
+            stdout_flusher._process_stdout_flushing()
+            condition_method.assert_called()
+            flush_stdout.assert_not_called()
 
     def test_no_required_stdout_flush(self):
         """
@@ -66,19 +47,15 @@ class TestStdoutFlusher:
             max_time_until_flushing, last_predict_time, current_time, flushing_expected=True
         )
 
-    @staticmethod
-    def _test_stdout_flush(max_wait_time, last_predict_time, current_time, flushing_expected):
+    @classmethod
+    def _test_stdout_flush(cls, max_wait_time, last_predict_time, current_time, flushing_expected):
         with patch.object(
             StdoutFlusher, "_current_time", side_effect=[last_predict_time, current_time]
         ), patch.object(StdoutFlusher, "_flush_stdout") as flush_stdout:
             stdout_flusher = StdoutFlusher(max_time_until_flushing=max_wait_time)
             stdout_flusher.set_last_activity_time()
-            stdout_flusher.start()
-            try:
-                time.sleep(max_wait_time * 2)
-                if flushing_expected:
-                    flush_stdout.assert_called()
-                else:
-                    flush_stdout.assert_not_called()
-            finally:
-                stdout_flusher.stop()
+            stdout_flusher._process_stdout_flushing()
+            if flushing_expected:
+                flush_stdout.assert_called()
+            else:
+                flush_stdout.assert_not_called()
