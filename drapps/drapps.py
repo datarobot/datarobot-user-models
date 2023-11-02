60772bcd7cf74c69af625b87e63b82636266d5b0
Illia Demchuk <illia.demchuk@datarobot.com>
2023-11-02 12:21:52 +0200

    Added copyright headers

--- drapps/drapps.py
+++ drapps/drapps.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+#
+#  Copyright 2023 DataRobot, Inc. and its affiliates.
+#
+#  All rights reserved.
+#  This is proprietary source code of DataRobot, Inc. and its affiliates.
+#  Released under the terms of DataRobot Tool and Utility Agreement.
+#
+
 import os
 import posixpath
 from pathlib import Path
