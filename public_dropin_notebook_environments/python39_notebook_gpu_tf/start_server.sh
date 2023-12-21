a9f27579a7249c406ebf15fd8d35c8637bdefc1f
Taras Lyapun <taras.lyapun@datarobot.com>
2023-12-21 08:05:08 +0200

    [NB-4998] Added custom environments for Python 3.11 and GPU with terraform

---
+++ public_dropin_notebook_environments/python39_notebook_gpu_tf/start_server.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    # Set default working directory if no argument is provided
+    WORKING_DIR="/home/notebooks"
+else
+    # Use the provided working directory
+    WORKING_DIR="$1"
+fi
+
+export WORKING_DIR
+
+cd /etc/system/kernel/agent || exit
+# shellcheck disable=SC1091
+source /etc/system/kernel/agent/.venv/bin/activate
+nohup uvicorn agent:app --host 0.0.0.0 --port 8889 &
+
+# shellcheck disable=SC1091
+source /etc/system/kernel/setup-ssh.sh
+source /etc/system/kernel/common-user-limits.sh
+cp -L /var/run/notebooks/ssh/authorized_keys/notebooks /etc/authorized_keys/ && chmod 600 /etc/authorized_keys/notebooks
+mkdir /etc/ssh/keys && cp -L /var/run/notebooks/ssh/keys/ssh_host_* /etc/ssh/keys/ && chmod 600 /etc/ssh/keys/ssh_host_*
+nohup /usr/sbin/sshd -D &
+
+cd /etc/system/kernel || exit
+# shellcheck disable=SC1091
+source /etc/system/kernel/.venv/bin/activate
+
+# setup the working directory for the kernel
+cd "$WORKING_DIR" || exit
+
+# setup ipython extensions
+cp -r /etc/ipython/ .ipython/
+
+# no trailing slash in the working dir path
+git config --global --add safe.directory "${WORKING_DIR%/}"
+
+exec jupyter kernelgateway --config=/etc/system/kernel/jupyter_kernel_gateway_config.py --debug
