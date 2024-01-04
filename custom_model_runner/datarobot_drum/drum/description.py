1a81b675350a4dc0f2452d6512958ef80793a377
Eric Shaw <eric.shaw@datarobot.com>
2024-01-04 11:23:14 -0500

    [MODEL-12854] CustomTasks Network Access: 5 - bump drum version to 1.10.17 (#906)
    * updated version and env id
    
    * fixed changelog

--- custom_model_runner/datarobot_drum/drum/description.py
+++ custom_model_runner/datarobot_drum/drum/description.py
@@ -4,6 +4,6 @@ All rights reserved.
 This is proprietary source code of DataRobot, Inc. and its affiliates.
 Released under the terms of DataRobot Tool and Utility Agreement.
 """
-version = "1.10.16"
+version = "1.10.17"
 __version__ = version
 project_name = "datarobot-drum"
