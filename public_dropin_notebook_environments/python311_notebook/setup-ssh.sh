7d542ad48339fdb460fe60e51c8a1aad1f97f1f4
Christopher Russell-Walker <christopher.russell-walker@datarobot.com>
2024-02-02 08:36:36 -0500

    [NB-5279] Fix Env Var issue by addressing script

--- public_dropin_notebook_environments/python311_notebook/setup-ssh.sh
+++ public_dropin_notebook_environments/python311_notebook/setup-ssh.sh
@@ -7,7 +7,12 @@ echo "Persisting container environment variables for sshd..."
     echo "# to ensure that they are exposed in ssh sessions"
     echo "# Ref: https://github.com/jenkinsci/docker-ssh-agent/issues/33#issuecomment-597367846"
     echo "set -a"
-    env | grep -E -v "^(PWD=|HOME=|TERM=|SHLVL=|LD_PRELOAD=|PS1=|_=|KUBERNETES_)"
+    env | grep -E -v "^(PWD=|HOME=|TERM=|SHLVL=|LD_PRELOAD=|PS1=|_=|KUBERNETES_)" | while read -r line; do
+      NAME=$(echo "$line" | cut -d'=' -f1)
+      VALUE=$(echo "$line" | cut -d'=' -f2-)
+      # Use eval to handle complex cases like export commands with spaces
+      echo "$NAME='$VALUE'"
+    done
     echo "set +a"
     # setup the working directory for terminal sessions
     echo "cd $WORKING_DIR"
