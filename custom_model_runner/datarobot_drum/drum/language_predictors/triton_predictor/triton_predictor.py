94af3d7d659024800101937aa8b95f26d8bae92f
Oleksandr Romaniuk <alexandr.romanyuk@datarobot.com>
2024-03-11 13:09:52 +0200

    update Triton predictor

--- custom_model_runner/datarobot_drum/drum/language_predictors/triton_predictor/triton_predictor.py
+++ custom_model_runner/datarobot_drum/drum/language_predictors/triton_predictor/triton_predictor.py
@@ -6,9 +6,11 @@ Released under the terms of DataRobot Tool and Utility Agreement.
 """
 import logging
 
+import requests
+
 from datarobot_drum.drum.adapters.model_adapters.python_model_adapter import RawPredictResponse
 from datarobot_drum.drum.common import SupportedPayloadFormats
-from datarobot_drum.drum.enum import LOGGER_NAME_PREFIX, PayloadFormat
+from datarobot_drum.drum.enum import LOGGER_NAME_PREFIX, PayloadFormat, UnstructuredDtoKeys
 from datarobot_drum.drum.exceptions import DrumCommonException
 from datarobot_drum.drum.language_predictors.base_language_predictor import BaseLanguagePredictor
 
@@ -19,9 +21,15 @@ class TritonPredictor(BaseLanguagePredictor):
     def __init__(self):
         super(TritonPredictor, self).__init__()
         self.logger = logging.getLogger(LOGGER_NAME_PREFIX + "." + __name__)
+        self.triton_host = None
+        self.triton_http_port = None
+        self.triton_grpc_port = None
 
     def mlpiper_configure(self, params):
         super(TritonPredictor, self).mlpiper_configure(params)
+        self.triton_host = params.get("triton_host")
+        self.triton_http_port = params.get("triton_http_port")
+        self.triton_http_port = params.get("triton_grpc_port")
 
     @property
     def supported_payload_formats(self):
@@ -36,8 +44,13 @@ class TritonPredictor(BaseLanguagePredictor):
         raise DrumCommonException("Predict structured is not supported")
 
     def predict_unstructured(self, data, **kwargs):
-        # TODO make an actual server call
-        return "hello world!", None
+        headers = kwargs.get(UnstructuredDtoKeys.HEADERS)
+        model_name = "densenet_onnx"
+        resp = requests.post(
+            f"{self.triton_host}:{self.triton_http_port}/v2/models/{model_name}/infer",
+            data=data, headers=headers
+        )
+        return resp.text, None
 
     def _transform(self, **kwargs):
         raise DrumCommonException("Transform feature is not supported")
