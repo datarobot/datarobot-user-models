d7993c76f7505e5203aa274fc9be37be1c00d8f7
Oleksandr Romaniuk <alexandr.romanyuk@datarobot.com>
2024-03-19 14:32:30 +0200

    update start_server.sh

--- public_dropin_gpu_environments/nemo_inference_ms/start_server.sh
+++ public_dropin_gpu_environments/nemo_inference_ms/start_server.sh
@@ -5,24 +5,46 @@
 # This is proprietary source code of DataRobot, Inc. and its affiliates.
 #
 # Released under the terms of DataRobot Tool and Utility Agreement.
-echo "Starting Custom Model environment with Triton inference server"
+echo "Starting Custom Model environment with NIM"
+set -e
 
 if [ "${ENABLE_CUSTOM_MODEL_RUNTIME_ENV_DUMP}" = 1 ]; then
     echo "Environment variables:"
     env
 
     echo
-    echo "Initializing NVIDIA..."
+    echo "Running NVIDIA init scripts..."
     echo
     /opt/nvidia/nvidia_entrypoint.sh /bin/true
 fi
 
+source /home/nemo/dr/bin/activate
 echo
-echo "Executing command: tritonserver --model-repository=${CODE_DIR}"
+echo "Starting DRUM server in background..."
 echo
-nohup tritonserver --model-repository=${MODEL_DIR} &
+nohup drum server --with-triton-server "$@" &
+
+if [[ -e $CODE_DIR/custom.py ]]; then
+    echo
+    echo "Running custom.py..."
+    echo
+    python ${CODE_DIR}/custom.py
+fi
+deactivate
 
 echo
-echo "Executing command: drum server $*"
+echo "Starting NeMo Inference Microservice..."
 echo
-exec drum server --with-triton-server "$@"
+export MODEL_DIR="${CODE_DIR}/model-store/"
+export MODEL_NAME=generic-llm
+
+export NEMO_PORT=9998
+export OPENAI_PORT=9999
+export HEALTH_PORT=8081
+
+exec nemollm_inference_ms --model $MODEL_NAME \
+    --log_level=info \
+    --health_port=$HEALTH_PORT \
+    --openai_port=$OPENAI_PORT \
+    --nemo_port=$NEMO_PORT \
+    --num_gpus=$(nvidia-smi -L | wc -l)
