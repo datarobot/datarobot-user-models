2a95345165f3f1fdda1604b7d2cce3765caa9367
Yakov Goldberg <yakov.goldberg@datarobot.com>
2024-04-15 10:32:18 -0700

    [RAPTOR-10658] pin uwsgi version installed into envs for tests (#980)

--- model_templates/triton_onnx_unstructured/client/datarobot-predict.py
+++ model_templates/triton_onnx_unstructured/client/datarobot-predict.py
@@ -18,10 +18,10 @@ import requests
 import json
 
 # See README.md on how to set up those keys
-API_URL = '<DATAROBOT_API_URL>'
-API_KEY = '<DATAROBOT_API_KEY>'
-DATAROBOT_KEY = '<DATAROBOT_KEY>'
-DEPLOYMENT_ID = '<DEPLOYMENT_ID>'
+API_URL = "<DATAROBOT_API_URL>"
+API_KEY = "<DATAROBOT_API_KEY>"
+DATAROBOT_KEY = "<DATAROBOT_KEY>"
+DEPLOYMENT_ID = "<DEPLOYMENT_ID>"
 
 # Don't change this. It is enforced server-side too.
 MAX_PREDICTION_FILE_SIZE_BYTES = 52428800  # 50 MB
@@ -36,7 +36,7 @@ def make_datarobot_deployment_unstructured_predictions(data, deployment_id, mime
     Make unstructured predictions on data provided using DataRobot deployment_id provided.
     See docs for details:
          https://docs.datarobot.com/en/docs/api/reference/predapi/dr-predapi.html
- 
+
     Parameters
     ----------
     data : bytes
@@ -48,26 +48,26 @@ def make_datarobot_deployment_unstructured_predictions(data, deployment_id, mime
         If mimetype starts with 'text/' or equal to 'application/json',
         data will be decoded with provided or default(UTF-8) charset
         and passed into the 'score_unstructured' hook implemented in custom.py provided with the model.
- 
+
         In case of other mimetype values data is treated as binary and passed without decoding.
     charset : str
         Charset should match the contents of the file, if file is text.
- 
+
     Returns
     -------
     data : bytes
         Arbitrary data returned by unstructured model.
- 
- 
+
+
     Raises
     ------
     DataRobotPredictionError if there are issues getting predictions from DataRobot
     """
     # Set HTTP headers. The charset should match the contents of the file.
     headers = {
-        'Content-Type': '{};charset={}'.format(mimetype, charset),
-        'Authorization': 'Bearer {}'.format(API_KEY),
-        'DataRobot-Key': DATAROBOT_KEY,
+        "Content-Type": "{};charset={}".format(mimetype, charset),
+        "Authorization": "Bearer {}".format(API_KEY),
+        "DataRobot-Key": DATAROBOT_KEY,
     }
 
     url = API_URL.format(deployment_id=deployment_id)
@@ -88,8 +88,7 @@ def _raise_dataroboterror_for_status(response):
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError:
-        err_msg = '{code} Error: {msg}'.format(
-            code=response.status_code, msg=response.text)
+        err_msg = "{code} Error: {msg}".format(code=response.status_code, msg=response.text)
         raise DataRobotPredictionError(err_msg)
 
 
@@ -107,15 +106,17 @@ def preprocess_image(img_path):
 
 
 def read_model_inference_header():
-    with open('model_header.json') as f:
+    with open("model_header.json") as f:
         json_header = json.load(f)
-        json_header_str = json.dumps(json_header) + '\n'
-    return json_header_str.encode('utf-8')
+        json_header_str = json.dumps(json_header) + "\n"
+    return json_header_str.encode("utf-8")
 
 
 def binary_response_as_numpy(response):
-    _, header_length = JSONDecoder().raw_decode(response.decode('utf-8'))
-    results = httpclient.InferenceServerClient.parse_response_body(response, header_length=header_length)
+    _, header_length = JSONDecoder().raw_decode(response.decode("utf-8"))
+    results = httpclient.InferenceServerClient.parse_response_body(
+        response, header_length=header_length
+    )
     return results.as_numpy("fc6_1").astype(str)
 
 
@@ -134,13 +135,16 @@ def main(filename, deployment_id, mimetype, charset):
 
     data_size = sys.getsizeof(data)
     if data_size >= MAX_PREDICTION_FILE_SIZE_BYTES:
-        print((
-                  'Input file is too large: {} bytes. '
-                  'Max allowed size is: {} bytes.'
-              ).format(data_size, MAX_PREDICTION_FILE_SIZE_BYTES))
+        print(
+            ("Input file is too large: {} bytes. " "Max allowed size is: {} bytes.").format(
+                data_size, MAX_PREDICTION_FILE_SIZE_BYTES
+            )
+        )
         return 1
     try:
-        response = make_datarobot_deployment_unstructured_predictions(data, deployment_id, mimetype, charset)
+        response = make_datarobot_deployment_unstructured_predictions(
+            data, deployment_id, mimetype, charset
+        )
     except DataRobotPredictionError as exc:
         print(exc)
         return 1
