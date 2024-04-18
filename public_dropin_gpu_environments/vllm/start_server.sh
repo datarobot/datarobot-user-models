ff75b96c49d29e731ae599a3df92221bf145079d
Erik Lattimore <elatt@users.noreply.github.com>
2024-04-17 22:22:20 -0400

    Don't report back usage stats

--- public_dropin_gpu_environments/vllm/start_server.sh
+++ public_dropin_gpu_environments/vllm/start_server.sh
@@ -9,6 +9,7 @@ echo "Starting Custom Model environment with vLLM"
 set -e
 
 export VLLM_CONFIGURE_LOGGING=0
+export VLLM_NO_USAGE_STATS=1
 
 # TODO: this is nvidia specific but vLLM supports other GPUs/TPUs/CPUs
 export GPU_COUNT=$(nvidia-smi -L | wc -l)
