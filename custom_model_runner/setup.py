359d4b45af38c7727530848a1e16e364fc2ca7c7
Yakov Goldberg <yakov.goldberg@datarobot.com>
2024-09-04 10:44:06 -0700

    [RAPTOR-11378] update Py support to Py>=3.8, bump version to 1.12.0 (#1113)

--- custom_model_runner/setup.py
+++ custom_model_runner/setup.py
@@ -47,10 +47,6 @@ setup(
     license="Apache License, Version 2.0",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -71,5 +67,5 @@ setup(
     scripts=["bin/drum"],
     install_requires=requirements,
     extras_require=extras_require,
-    python_requires=">=3.4,<3.12",
+    python_requires=">=3.8,<3.12",
 )
