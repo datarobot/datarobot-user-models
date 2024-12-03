0b015590fcbcc2bcb2aba8e2e94ec61b96ea9d22
Taras Nek <taras.nek@datarobot.com>
2024-12-03 13:42:37 +0200

    fix

--- tests/fixtures/geo_point_custom.py
+++ tests/fixtures/geo_point_custom.py
@@ -4,6 +4,8 @@ All rights reserved.
 This is proprietary source code of DataRobot, Inc. and its affiliates.
 Released under the terms of DataRobot Tool and Utility Agreement.
 """
+import pandas as pd
+
 
 def load_model(input_dir):
     _ = input_dir
@@ -12,4 +14,6 @@ def load_model(input_dir):
 
 def score(data, model, **kwargs):
     _ = model
-    return data[["latitude", "longitude"]]
+    predictions_data = pd.DataFrame({"Predictions": data[["latitude", "longitude"]]})
+
+    return predictions_data
