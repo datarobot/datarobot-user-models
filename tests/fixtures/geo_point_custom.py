e7089df79f0153c2b6178502c300e717bbaf995f
Taras Nek <taras.nek@datarobot.com>
2024-12-09 12:28:01 +0200

    Fix tests

--- tests/fixtures/geo_point_custom.py
+++ tests/fixtures/geo_point_custom.py
@@ -12,16 +12,14 @@ def load_model(input_dir):
     _ = input_dir
     return "dummy"
 
+
 def transform(data, model):
     data = data.fillna(0)
     return data
 
 
 def score(data, model, **kwargs):
-    predictions = pd.DataFrame([1.0] * data.shape[0], columns=["Predictions"])
-    return predictions
-
-
-def score(data, model, **kwargs):
     _ = model
-    return pd.DataFrame([f"POINT({d.latitude}, {d.longitude})" for _, d in df.iterrows()], columns=["Prediction"])
+    return pd.DataFrame(
+        [f"POINT({d.latitude}, {d.longitude})" for _, d in data.iterrows()], columns=["Prediction"]
+    )
