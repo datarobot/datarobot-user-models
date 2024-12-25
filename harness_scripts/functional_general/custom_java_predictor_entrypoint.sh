e573168fe5a6763de8f61d41fcbe1c199c6876d0
Zohar Mizrahi <zohar.mizrahi@datarobot.com>
2024-12-25 18:48:22 +0200

    [RAPTOR-11814] Improve Harness scripts for better robustness and independence

--- harness_scripts/functional_general/custom_java_predictor_entrypoint.sh
+++ harness_scripts/functional_general/custom_java_predictor_entrypoint.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-cd ./tests/functional/custom_java_predictor
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. ${script_dir}/../common/create_and_source_venv.sh
+
+pushd ./tests/functional/custom_java_predictor
 mvn package
-cd -
-cd custom_model_runner
+popd
+
+pushd custom_model_runner
 echo "== Build java entrypoint, base predictor and install DRUM from source =="
 make java_components
 pip install .
-cd -
+popd
+
 pytest tests/functional/test_inference_custom_java_predictor.py
