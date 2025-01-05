67082540c8d7209e56b283056493a6775dbec62b
Zohar Mizrahi <zohar.mizrahi@datarobot.com>
2025-01-05 09:20:35 +0200

    [RAPTOR-11814] Improve Harness scripts for better robustness and independence (#1207)
    * [RAPTOR-11814] Improve Harness scripts for better robustness and independence
    
    * Install drum for source + more print outs

--- harness_scripts/base_check/integration_tests_entrypoint.sh
+++ harness_scripts/base_check/integration_tests_entrypoint.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 set -exuo pipefail
-cd custom_model_runner
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. ${script_dir}/../common/create_and_source_venv.sh
+
+pushd custom_model_runner
 make
-cd -
-python3 -m venv /tmp/venv
-. /tmp/venv/bin/activate
+popd  # custom_model_runner
+
 pip3 install -U pip
 pip3 install -r requirements_test_unit.txt
 pip3 install -e custom_model_runner/
--- harness_scripts/functional_general/general_tests_entrypoint.sh
+++ harness_scripts/functional_general/general_tests_entrypoint.sh
@@ -6,13 +6,16 @@ docker login -u datarobotread2 -p $DOCKER_HUB_SECRET
 
 echo "== Build image for tests =="
 cp -r custom_model_runner/ public_dropin_environments/python3_sklearn/
-cd public_dropin_environments/python3_sklearn/
-echo -e "RUN pip uninstall -y datarobot-drum\nCOPY ./custom_model_runner /tmp/custom_model_runner\nRUN pip install /tmp/custom_model_runner" >> Dockerfile
 
+pushd public_dropin_environments/python3_sklearn/
+echo -e "RUN pip uninstall -y datarobot-drum\nCOPY ./custom_model_runner /tmp/custom_model_runner\nRUN pip install /tmp/custom_model_runner" >> Dockerfile
 docker build -t python3_sklearn_test_env .
-cd -
+popd
 
 docker images
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. ${script_dir}/../common/create_and_source_venv.sh
 
 echo "== Preparing to test =="
 echo "Installing requirements for all the tests: requirements_test.txt"
@@ -21,10 +24,10 @@ pip install -r requirements_test.txt
 echo "== Uninstall drum =="
 pip uninstall -y datarobot-drum
 
-cd custom_model_runner
+pushd custom_model_runner
 echo "== Install drum from source =="
 pip install .
-cd -
+popd
 
 pytest tests/functional/test_mlops_monitoring.py::TestMLOpsMonitoring::test_drum_unstructured_model_embedded_monitoring_in_sklearn_env
 pytest tests/functional/ -k "not test_inference_custom_java_predictor.py and not test_mlops_monitoring.py" -m "not sequential"
