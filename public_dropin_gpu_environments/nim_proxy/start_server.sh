7f97babe183728ee62fbcd7b91f23fe7278fbf2c
Oleksandr Romaniuk <alexandr.romanyuk@datarobot.com>
2025-02-07 17:33:09 +0200

    move unexpected gpu count check

---
+++ public_dropin_gpu_environments/nim_proxy/start_server.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright 2024 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+echo "Starting Custom Model environment with NIM"
+set -e
+
+
+if [ "${ENABLE_CUSTOM_MODEL_RUNTIME_ENV_DUMP}" = 1 ]; then
+    echo "Environment variables:"
+    env
+fi
+
+echo
+echo "Starting DRUM server..."
+echo
+exec drum server --gpu-predictor=nim --multi-container-deployment --logging-level=info "$@"
