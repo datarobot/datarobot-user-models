d0b598bcaed38924fc2ca00cfe8162687ceea495
Bogdan Klichuk <bogdan.klichuk@datarobot.com>
2025-04-05 19:39:52 +0300

    .

---
+++ public_dropin_gpu_environments/vllm/ff/start_server.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Copyright 2024 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+echo "Starting Custom Model environment with vLLM"
+set -e
+
+export GPU_COUNT=$(nvidia-smi -L | wc -l)
+echo "GPU count: $GPU_COUNT"
+
+echo
+echo "Starting DRUM server..."
+echo
+
+. ${DATAROBOT_VENV_PATH}/bin/activate
+exec drum server --gpu-predictor=vllm --logging-level=info "$@"
