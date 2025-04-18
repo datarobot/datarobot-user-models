7d22e731d8efb1017034b42ab5200aac1478e384
Scott Oglesby <scott.oglesby@datarobot.com>
2025-04-18 16:04:51 -0700

    added prediction server tests

--- tests/unit/datarobot_drum/drum/test_prediction_server.py
+++ tests/unit/datarobot_drum/drum/test_prediction_server.py
@@ -1,4 +1,5 @@
 import os
+from unittest.mock import ANY
 from unittest.mock import Mock, patch
 
 import httpx
@@ -10,6 +11,7 @@ from openai import OpenAI, Stream
 from openai.types.chat import (
     ChatCompletion,
 )
+from openai.types.model import Model
 from werkzeug.exceptions import BadRequest
 
 from datarobot_drum.drum.enum import RunLanguage, TargetType
@@ -17,6 +19,7 @@ from datarobot_drum.drum.lazy_loading.lazy_loading_handler import LazyLoadingHan
 from datarobot_drum.drum.root_predictors.prediction_server import PredictionServer
 from datarobot_drum.drum.server import _create_flask_app
 from tests.unit.datarobot_drum.drum.chat_utils import create_completion, create_completion_chunks
+from tests.unit.datarobot_drum.drum.helpers import inject_runtime_parameter
 
 
 @pytest.fixture
@@ -38,6 +41,22 @@ def test_flask_app():
 
 @pytest.fixture
 def prediction_server(test_flask_app, chat_python_model_adapter):
+    with patch.dict(os.environ, {"TARGET_NAME": "target"}), patch(
+        "datarobot_drum.drum.language_predictors.python_predictor.python_predictor.PythonPredictor._init_mlops"
+    ), patch.object(LazyLoadingHandler, "download_lazy_loading_files"):
+        params = {
+            "run_language": RunLanguage.PYTHON,
+            "target_type": TargetType.TEXT_GENERATION,
+            "deployment_config": None,
+            "__custom_model_path__": "/non-existing-path-to-avoid-loading-unwanted-artifacts",
+        }
+        server = PredictionServer(params)
+        server._predictor._mlops = Mock()
+        server.materialize()
+
+
+@pytest.fixture
+def list_models_prediction_server(test_flask_app, list_models_python_model_adapter):
     with patch.dict(os.environ, {"TARGET_NAME": "target"}), patch(
         "datarobot_drum.drum.language_predictors.python_predictor.python_predictor.PythonPredictor._init_mlops"
     ), patch.object(LazyLoadingHandler, "download_lazy_loading_files"):
@@ -145,6 +164,63 @@ def test_prediction_server_chat_unimplemented(openai_client):
         )
 
 
+@pytest.mark.usefixtures("prediction_server")
+def test_prediction_server_list_llm_models_no_hook_no_rtp(openai_client, chat_python_model_adapter):
+    """Attempt to list supported LLM models where no hook exists and no runtime parameter exists."""
+    response = openai_client.models.list()
+    assert response.object == "list"
+    assert response.data == []
+
+
+@pytest.mark.usefixtures("prediction_server")
+def test_prediction_server_list_llm_models_no_hook_with_rtp(
+    openai_client, chat_python_model_adapter
+):
+    """Attempt to list supported LLM models where no hook exists but a runtime parameter exists."""
+    model_id = "test_no_hook_with_rtp"
+    inject_runtime_parameter("LLM_ID", model_id)
+    response = openai_client.models.list()
+    assert response.object == "list"
+    assert len(response.data) == 1
+    assert response.data[0].to_dict() == {
+        "id": model_id,
+        "object": "model",
+        "created": ANY,
+        "owned_by": "DataRobot",
+    }
+
+
+@pytest.mark.usefixtures("list_models_prediction_server")
+def test_prediction_server_list_llm_models_with_hook(
+    openai_client, list_models_python_model_adapter
+):
+    """List supported LLM models where a hook exists."""
+    model_id = "test_with_hook model"
+    owned_by = "test_with_hook owner"
+
+    def models_hook(model):
+        return [
+            Model(
+                id=model_id,
+                created=1744854432,
+                object="model",
+                owned_by=owned_by,
+            )
+        ]
+
+    list_models_python_model_adapter.models_hook = models_hook
+    inject_runtime_parameter("LLM_ID", "model_id_from_RTP")
+    response = openai_client.models.list()
+    assert response.object == "list"
+    assert len(response.data) == 1
+    assert response.data[0].to_dict() == {
+        "id": model_id,
+        "object": "model",
+        "created": ANY,
+        "owned_by": owned_by,
+    }
+
+
 @pytest.mark.usefixtures("non_textgen_prediction_server")
 def test_prediction_server_list_llm_models_unsupported(openai_client):
     """Attempt to list supported LLM models with a non-textgen model."""
@@ -152,7 +228,6 @@ def test_prediction_server_list_llm_models_unsupported(openai_client):
         _ = openai_client.models.list()
 
 
-@pytest.mark.usefixtures("prediction_server")
 @pytest.mark.parametrize("use_generator", [True, False])
 def test_streaming(openai_client, chat_python_model_adapter, use_generator):
     chunks = create_completion_chunks(["How", "are", "you", "doing"])
