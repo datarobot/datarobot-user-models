743fdd560ab7af8a1d8d9b5a61a86af928453d71
mjnitz02 <matt.nitzken@datarobot.com>
2025-05-13 14:07:42 -0400

    Update GenAI Agents environment to latest

--- public_dropin_environments/python311_genai_agents/run_agent.py
+++ public_dropin_environments/python311_genai_agents/run_agent.py
@@ -1,139 +1,160 @@
-# Copyright 2025 DataRobot, Inc. and its affiliates.
+# Copyright 2025 DataRobot, Inc.
 #
-# All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This is proprietary source code of DataRobot, Inc. and its affiliates.
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-# Released under the terms of DataRobot Tool and Utility Agreement.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # pylint: skip-file
 
 import argparse
 import json
+import logging
 import os
+import sys
+from typing import Any, cast
 
 import requests
-from custom import chat
-from custom import load_model
 from datarobot_drum.drum.enum import TargetType
 from datarobot_drum.drum.root_predictors.drum_server_utils import DrumServerRun
 from openai import OpenAI
-from openai.types.chat import ChatCompletionSystemMessageParam
-from openai.types.chat import ChatCompletionUserMessageParam
-from openai.types.chat.completion_create_params import CompletionCreateParamsNonStreaming
+from openai.types.chat import (
+    ChatCompletion,
+    ChatCompletionSystemMessageParam,
+    ChatCompletionUserMessageParam,
+)
+from openai.types.chat.completion_create_params import (
+    CompletionCreateParamsNonStreaming,
+)
+
+root = logging.getLogger()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--store_output", action="store_true", help="Store output in a file")
 parser.add_argument(
-    "--use_drum", action="store_true", help="Use DRUM for execution instead of direct execution"
+    "--user_prompt", type=str, default="", help="user_prompt for chat endpoint"
 )
-parser.add_argument("--user_prompt", type=str, default="", help="user_prompt for chat endpoint")
-parser.add_argument("--extra_body", type=str, default="", help="extra_body for chat endpoint")
+parser.add_argument(
+    "--extra_body", type=str, default="", help="extra_body for chat endpoint"
+)
+parser.add_argument(
+    "--custom_model_dir",
+    type=str,
+    default="",
+    help="directory containing custom.py location",
+)
+parser.add_argument(
+    "--output_path", type=str, default="", help="json output file location"
+)
 args = parser.parse_args()
 
 
-class RunAgent:
-    """
-    This class is responsible for running the agent. It can run in two modes:
-        - :code:`execute_direct`: Directly using the chat endpoint.
-        - :code:`execute_drum`: Using the DRUM server.
-    """
+def setup_logging(
+    logger: logging.Logger, output_path: str, log_level: int = logging.INFO
+) -> None:
+    if len(output_path) == 0:
+        output_path = "output.log"
+    else:
+        output_path = f"{output_path}.log"
 
-    @property
-    def code_dir(self):
-        return "/home/notebooks/storage/"
+    logger.setLevel(log_level)
+    handler_stream = logging.StreamHandler(sys.stdout)
+    handler_stream.setLevel(log_level)
+    formatter = logging.Formatter("%(message)s")
+    handler_stream.setFormatter(formatter)
 
-    @staticmethod
-    def construct_prompt(user_prompt, extra_body, merge_extra_body=True):
-        extra_body_params = json.loads(extra_body) if extra_body else {}
-        completion_create_params = CompletionCreateParamsNonStreaming(
-            model="datarobot-deployed-llm",
-            messages=[
-                ChatCompletionSystemMessageParam(
-                    content="You are a helpful assistant",
-                    role="system",
-                ),
-                ChatCompletionUserMessageParam(
-                    content=user_prompt,
-                    role="user",
-                ),
-            ],
-            n=1,
-            temperature=0.01,
-            extra_body=extra_body_params,
+    if os.path.exists(output_path):
+        os.remove(output_path)
+    handler_file = logging.FileHandler(output_path)
+    handler_file.setLevel(log_level)
+    formatter_file = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler_file.setFormatter(formatter_file)
+
+    logger.addHandler(handler_stream)
+    logger.addHandler(handler_file)
+
+
+def construct_prompt(user_prompt: str, extra_body: str) -> Any:
+    extra_body_params = json.loads(extra_body) if extra_body else {}
+    completion_create_params = CompletionCreateParamsNonStreaming(
+        model="datarobot-deployed-llm",
+        messages=[
+            ChatCompletionSystemMessageParam(
+                content="You are a helpful assistant",
+                role="system",
+            ),
+            ChatCompletionUserMessageParam(
+                content=user_prompt,
+                role="user",
+            ),
+        ],
+        n=1,
+        temperature=0.01,
+        extra_body=extra_body_params,  # type: ignore[typeddict-unknown-key]
+    )
+    return completion_create_params
+
+
+def execute_drum(
+    user_prompt: str, extra_body: str, custom_model_dir: str, output_path: str
+) -> ChatCompletion:
+    root.info("Executing agent as [chat] endpoint. DRUM Executor.")
+    root.info("Starting DRUM server.")
+    with DrumServerRun(
+        target_type=TargetType.TEXT_GENERATION.value,
+        labels=None,
+        custom_model_dir=custom_model_dir,
+        with_error_server=True,
+        production=False,
+        verbose=True,
+        logging_level="info",
+        target_name="response",
+        wait_for_server_timeout=360,
+        port=8191,
+        stream_output=True,
+    ) as drum_runner:
+        root.info("Verifying DRUM server.")
+        response = requests.get(drum_runner.url_server_address)
+        if not response.ok:
+            raise RuntimeError("Server failed to start")
+
+        # Use a standard OpenAI client to call the DRUM server. This mirrors the behavior of a deployed agent.
+        root.info("Building prompt.")
+        client = OpenAI(
+            base_url=drum_runner.url_server_address,
+            api_key="not-required",
+            max_retries=0,
         )
-        if merge_extra_body:
-            completion_create_params.update(**extra_body_params)
-        return completion_create_params
+        completion_create_params = construct_prompt(user_prompt, extra_body)
 
-    def execute_direct(self, user_prompt, extra_body):
-        print("Executing agent as [chat] endpoint. Local Executor.")
-        completion_create_params = self.construct_prompt(user_prompt, extra_body)
+        root.info("Executing Agent.")
+        completion = client.chat.completions.create(**completion_create_params)
 
-        # Use direct execution of the agent. This is more straightforward to debug agent code related issues.
-        model = load_model(code_dir=self.code_dir)
-        chat_result = chat(completion_create_params, model)
+    # Continue outside the context manager to ensure the server is stopped and logs
+    # are flushed before we write the output
+    root.info(f"Storing result: {output_path}")
+    if len(output_path) == 0:
+        output_path = os.path.join(custom_model_dir, "output.json")
+    with open(output_path, "w") as fp:
+        fp.write(completion.to_json())
 
-        return chat_result
-
-    def execute_drum(self, user_prompt, extra_body):
-        print("Executing agent as [chat] endpoint. DRUM Executor.")
-        print("NOTE: Realtime logging will be delayed in terminal and displayed after execution.")
-
-        # This logic spools an ephemeral DRUM server to run the agent
-        os.environ[
-            "MLOPS_RUNTIME_PARAM_CUSTOM_MODEL_WORKERS"
-        ] = '{"type": "numeric", "payload": 10}'
-        os.environ["MLOPS_RUNTIME_PARAM_model"] = json.dumps(
-            {
-                "type": "string",
-                "payload": "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-            }
-        )
-        os.environ[
-            "MLOPS_RUNTIME_PARAM_prompt_column_name"
-        ] = '{"type":"string","payload":"user_prompt"}'
-        os.environ[
-            "MLOPS_RUNTIME_PARAM_system_prompt"
-        ] = '{"type":"string","payload":"You are a helpful assistant"}'
-        os.environ["MLOPS_RUNTIME_PARAM_temperature"] = '{"type":"numeric","payload":0.01}'
-        with DrumServerRun(
-            target_type=TargetType.TEXT_GENERATION.value,
-            labels=None,
-            custom_model_dir=self.code_dir,
-            with_error_server=True,
-            production=False,
-            logging_level="info",
-            target_name="response",
-            wait_for_server_timeout=360,
-            port=8191,
-        ) as drum_runner:
-            response = requests.get(drum_runner.url_server_address)
-            if not response.ok:
-                raise RuntimeError("Server failed to start")
-
-            # Use a standard OpenAI client to call the DRUM server. This mirrors the behavior of a deployed agent.
-            client = OpenAI(
-                base_url=drum_runner.url_server_address, api_key="not-required", max_retries=0
-            )
-            completion_create_params = self.construct_prompt(
-                user_prompt, extra_body, merge_extra_body=False
-            )
-            completion = client.chat.completions.create(**completion_create_params)
-
-            return completion
-
-    def store_output(self, chat_result):
-        with open(os.path.join(self.code_dir, "output.json"), "w") as fp:
-            fp.write(chat_result.to_json())
+    root.info(completion.to_json())
+    return cast(ChatCompletion, completion)
 
 
-# This is the main entry point for the script
-runner = RunAgent()
-if args.use_drum:
-    result = runner.execute_drum(user_prompt=args.user_prompt, extra_body=args.extra_body)
-else:
-    result = runner.execute_direct(user_prompt=args.user_prompt, extra_body=args.extra_body)
-
-# Store results to file if requested
-if args.store_output:
-    runner.store_output(result)
+# Agent execution
+if len(args.custom_model_dir) == 0:
+    args.custom_model_dir = os.path.join(os.getcwd(), "custom_model")
+setup_logging(logger=root, output_path=args.output_path, log_level=logging.INFO)
+result = execute_drum(
+    user_prompt=args.user_prompt,
+    extra_body=args.extra_body,
+    custom_model_dir=args.custom_model_dir,
+    output_path=args.output_path,
+)
