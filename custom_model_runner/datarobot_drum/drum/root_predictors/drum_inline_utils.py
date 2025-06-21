b111417862db6fc339e9bf11a30ba8b513b819e7
Bogdan Klichuk <bogdan.klichuk@datarobot.com>
2025-06-22 01:46:55 +0300

    type annotation

--- custom_model_runner/datarobot_drum/drum/root_predictors/drum_inline_utils.py
+++ custom_model_runner/datarobot_drum/drum/root_predictors/drum_inline_utils.py
@@ -23,17 +23,31 @@ with drum_inline_predictor(target_type=TargetType.AGENTIC_WORKFLOW.value, custom
 import contextlib
 import os
 import tempfile
+from typing import Generator, List
 
 from datarobot_drum.drum.args_parser import CMRunnerArgsRegistry
 from datarobot_drum.drum.common import setup_required_environment_variables, setup_tracer
 from datarobot_drum.drum.drum import CMRunner
+from datarobot_drum.drum.language_predictors.base_language_predictor import BaseLanguagePredictor
 from datarobot_drum.drum.runtime import DrumRuntime
 from datarobot_drum.drum.root_predictors.generic_predictor import GenericPredictorComponent
 from datarobot_drum.runtime_parameters.runtime_parameters import RuntimeParameters
 
 
 @contextlib.contextmanager
-def drum_inline_predictor(target_type, custom_model_dir, target_name, *cmd_args):
+def drum_inline_predictor(
+    target_type: str, custom_model_dir: str, target_name: str, *cmd_args: List[str]
+) -> Generator[BaseLanguagePredictor, None, None]:
+    """
+    Drum run for a custom model code definition. Yields a predictor, ready to work with.
+    Caller can work with the predictor directly.
+
+    :param target_type: Target type.
+    :param custom_model_dir: Directory where the custom model code artifacts are located.
+    :param target_name: Name of the target
+    :param cmd_args: Extra command line arguments
+    :return:
+    """
     with DrumRuntime() as runtime, tempfile.NamedTemporaryFile(mode="wb") as tf:
         # setup
 
