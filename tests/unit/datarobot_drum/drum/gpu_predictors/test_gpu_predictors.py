6eb0313de2dbcab85e1fe4bc94651062bb81910c
Bogdan Klichuk <bogdan.klichuk@datarobot.com>
2025-06-25 22:23:12 +0300

    [RAPTOR-14025] Allow override nim server details, release v1.16.10.post1

--- tests/unit/datarobot_drum/drum/gpu_predictors/test_gpu_predictors.py
+++ tests/unit/datarobot_drum/drum/gpu_predictors/test_gpu_predictors.py
@@ -1,9 +1,17 @@
+import json
+import os
 import typing
+from unittest.mock import patch
+
 import pytest
+
+from datarobot_drum import RuntimeParameters
 from datarobot_drum.drum.enum import TargetType
 
 from datarobot_drum.drum.gpu_predictors.base import BaseOpenAiGpuPredictor
+from datarobot_drum.drum.gpu_predictors.nim_predictor import NIMPredictor
 from datarobot_drum.drum.root_predictors.drum_server_utils import DrumServerProcess
+from datarobot_drum.runtime_parameters.runtime_parameters_schema import RuntimeParameterTypes
 
 
 class TestGPUPredictor(BaseOpenAiGpuPredictor):
@@ -28,6 +36,20 @@ def mock_target_name_env_var(monkeypatch):
     monkeypatch.delenv("TARGET_NAME")
 
 
+@pytest.fixture
+def mock_openai_host_env_var(monkeypatch):
+    monkeypatch.setenv("OPENAI_HOST", "mocked.openai.host")
+    yield
+    monkeypatch.delenv("OPENAI_HOST")
+
+
+@pytest.fixture
+def mock_openai_port_env_var(monkeypatch):
+    monkeypatch.setenv("OPENAI_PORT", "45678")
+    yield
+    monkeypatch.delenv("OPENAI_PORT")
+
+
 @pytest.mark.parametrize("target_type", list(TargetType))
 def test_supports_chat(mock_target_name_env_var, target_type):
     predictor = TestGPUPredictor()
@@ -40,3 +62,79 @@ def test_supports_chat(mock_target_name_env_var, target_type):
         assert predictor.supports_chat()
     else:
         assert not predictor.supports_chat()
+
+    @pytest.mark.parametrize(
+        "runtime_param_type, payload",
+        [
+            (RuntimeParameterTypes.STRING, "Some string value"),
+            (RuntimeParameterTypes.BOOLEAN, True),
+            (RuntimeParameterTypes.NUMERIC, 10),
+            (
+                RuntimeParameterTypes.CREDENTIAL,
+                {
+                    "credentialType": "s3",
+                    "region": "us-west",
+                    "awsAccessKeyId": "123aaa",
+                    "awsSecretAccessKey": "3425sdd",
+                    "awsSessionToken": "12345abcde",
+                },
+            ),
+        ],
+    )
+    def test_valid(self, runtime_param_type, payload):
+        runtime_param_name = "AAA"
+        namespaced_runtime_param_name = RuntimeParameters.namespaced_param_name(runtime_param_name)
+        runtime_param_env_value = json.dumps({"type": runtime_param_type.value, "payload": payload})
+        with patch.dict(os.environ, {namespaced_runtime_param_name: runtime_param_env_value}):
+            assert RuntimeParameters.has(runtime_param_name)
+            assert RuntimeParameters.get(runtime_param_name) == payload
+
+
+def rt_param_name(name):
+    return RuntimeParameters.namespaced_param_name(name)
+
+
+def rt_param_value(type, value):
+    return json.dumps(
+        {
+            "type": type,
+            "payload": value,
+        }
+    )
+
+
+def rt_param_str_value(value):
+    return rt_param_value(RuntimeParameterTypes.STRING.value, value)
+
+
+class TestNIMPredictor:
+    def test_nim_predictor_created_with_default_values(self):
+        predictor = NIMPredictor()
+        assert predictor.health_route == "/v1/health/ready"
+        assert predictor.openai_port == "9999"
+        assert predictor.openai_host == "localhost"
+
+    @pytest.mark.usefixtures("mock_openai_host_env_var", "mock_openai_port_env_var")
+    def test_nim_predictor_created_with_values_from_env_vars(self):
+        predictor = NIMPredictor()
+        assert predictor.health_route == "/v1/health/ready"
+        assert predictor.openai_port == "45678"
+        assert predictor.openai_host == "mocked.openai.host"
+
+    @pytest.mark.usefixtures("mock_openai_host_env_var", "mock_openai_port_env_var")
+    def test_nim_predictor_created_with_values_from_runtime_params_and_takes_precedence_over_env_vars(
+        self,
+    ):
+        with patch.dict(
+            os.environ,
+            {
+                rt_param_name("DR_NIM_HEALTH_ROUTE"): rt_param_str_value("/overriden/health/route"),
+                rt_param_name("DR_NIM_SERVER_HOST"): rt_param_str_value("mocked.host"),
+                rt_param_name("DR_NIM_SERVER_PORT"): rt_param_str_value("mocked.port"),
+            },
+        ):
+            predictor = NIMPredictor()
+
+            assert predictor.health_route == "/overriden/health/route"
+            assert predictor.openai_port == "mocked.port"
+            assert predictor.openai_host == "mocked.host"
