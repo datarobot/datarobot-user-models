69bebcee20933f10815d0ce35973309bea6c0715
mjnitz02 <matt.nitzken@datarobot.com>
2025-08-06 14:31:36 -0400

    Refactor setup to new file

--- custom_model_runner/datarobot_drum/drum/common.py
+++ custom_model_runner/datarobot_drum/drum/common.py
@@ -15,7 +15,6 @@ from urllib.parse import urlparse, urlunparse
 from contextlib import contextmanager
 from pathlib import Path
 
-from datarobot_drum.drum.args_parser import CMRunnerArgsRegistry
 from datarobot_drum.drum.enum import (
     LOGGER_NAME_PREFIX,
     MODEL_CONFIG_FILENAME,
@@ -23,11 +22,6 @@ from datarobot_drum.drum.enum import (
     PayloadFormat,
 )
 from datarobot_drum.drum.exceptions import DrumCommonException
-from datarobot_drum.drum.lazy_loading.lazy_loading_handler import LazyLoadingHandler
-from datarobot_drum.runtime_parameters.runtime_parameters import (
-    RuntimeParametersLoader,
-    RuntimeParameters,
-)
 from opentelemetry import trace, context
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
@@ -235,46 +229,3 @@ def extract_chat_response_attributes(response):
         # last completion wins
         attributes["gen_ai.completion"] = m.get("content")
     return attributes
-
-
-def setup_options(args=None):
-    """
-    Setup options for the Drum runtime.
-    This function is used to set up the command line arguments and options
-    for the Drum runtime, including environment variables and maximum workers.
-    """
-    arg_parser = CMRunnerArgsRegistry.get_arg_parser()
-
-    try:
-        import argcomplete
-    except ImportError:
-        print(
-            "WARNING: autocompletion of arguments is not supported "
-            "as 'argcomplete' package is not found",
-            file=sys.stderr,
-        )
-    else:
-        # argcomplete call should be as close to the beginning as possible
-        argcomplete.autocomplete(arg_parser)
-
-    CMRunnerArgsRegistry.extend_sys_argv_with_env_vars()
-
-    options = arg_parser.parse_args(args)
-    CMRunnerArgsRegistry.verify_options(options)
-
-    if "runtime_params_file" in options and options.runtime_params_file:
-        loader = RuntimeParametersLoader(options.runtime_params_file, options.code_dir)
-        loader.setup_environment_variables()
-
-    if "lazy_loading_file" in options and options.lazy_loading_file:
-        LazyLoadingHandler.setup_environment_variables_from_values_file(options.lazy_loading_file)
-
-    """Set max workers from runtime parameters if available."""
-    if RuntimeParameters.has("CUSTOM_MODEL_WORKERS"):
-        options.max_workers = RuntimeParameters.get("CUSTOM_MODEL_WORKERS")
-    elif "max_workers" not in options:
-        options.max_workers = 1  # Default to 1 worker if not specified
-    else:
-        options.max_workers = int(options.max_workers)
-
-    return options
