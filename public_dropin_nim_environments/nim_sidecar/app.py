e75d3e4f5e7afa62e93cce0e7f805b46a1089528
Sergey Gavrenkov <sergey.gavrenkov@datarobot.com>
2025-08-20 23:49:08 -0400

    working

--- public_dropin_nim_environments/nim_sidecar/app.py
+++ public_dropin_nim_environments/nim_sidecar/app.py
@@ -1,6 +1,9 @@
 # Import DRUM's WSGI application
-import sys
 
+from gevent import monkey
+monkey.patch_all()
+
+import sys
 from datarobot_drum.drum.main import main
 from datarobot_drum.drum.server import create_flask_app
 
--- public_dropin_nim_environments/nim_sidecar/start_server.sh
+++ public_dropin_nim_environments/nim_sidecar/start_server.sh
@@ -17,5 +17,5 @@ echo
 echo "Starting DRUM server..."
 echo
 
-exec gunicorn app:app  -k gthread --workers=8 --bind=0.0.0.0:8080 --backlog=512 --threads 4 --timeout 120 --max-requests 1000 --max-requests-jitter 100 --log-level=info --access-logfile - --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'& #works
+exec gunicorn app:app --worker-class gevent --workers=8 --bind=0.0.0.0:8080 --backlog=512 --timeout 120 --max-requests 700 --max-requests-jitter 400 --log-level=info --access-logfile - --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'& #works
 wait
