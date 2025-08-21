52541cd0617261d69598635f9308061b4ceac415
Sergey Gavrenkov <sergey.gavrenkov@datarobot.com>
2025-08-21 16:43:59 -0400

    working

--- public_dropin_nim_environments/nim_sidecar/app.py
+++ public_dropin_nim_environments/nim_sidecar/app.py
@@ -1,8 +1,5 @@
 # Import DRUM's WSGI application
 
-from gevent import monkey
-monkey.patch_all()
-
 import sys
 from datarobot_drum.drum.main import main
 from datarobot_drum.drum.server import create_flask_app
