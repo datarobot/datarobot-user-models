4fe44ec159a513b8ca497e1a70f449e2850b2b72
Sergey Gavrenkov <86368350+s-gavrenkov@users.noreply.github.com>
2025-09-22 22:06:36 -0400

    bump DRUM and fix run gunicorn only in server mode (#1665)

--- custom_model_runner/datarobot_drum/drum/entry_point.py
+++ custom_model_runner/datarobot_drum/drum/entry_point.py
@@ -1,12 +1,19 @@
 from datarobot_drum.drum.gunicorn.run_gunicorn import main_gunicorn
 from datarobot_drum.drum.main import main
+import sys
 from datarobot_drum import RuntimeParameters
 
 
 def run_drum_server():
+    cmd_args = sys.argv
+    is_server = False
+    if len(cmd_args) > 1 and cmd_args[1] == "server":
+        is_server = True
+
     if (
         RuntimeParameters.has("DRUM_SERVER_TYPE")
         and str(RuntimeParameters.get("DRUM_SERVER_TYPE")).lower() == "gunicorn"
+        and is_server
     ):
         main_gunicorn()
     else:
