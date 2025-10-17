c0bccd1bc2967bae919093e5f71a23c3ed28ff53
Roman Midianyi <roman.midianyi@datarobot.com>
2025-10-17 15:36:00 +0300

    [BUZZOK-27814][BUZZOK-27812] Install extra agent dependecies in runtime.

--- public_dropin_environments/python311_genai_agents/start_server_drum.sh
+++ public_dropin_environments/python311_genai_agents/start_server_drum.sh
@@ -12,6 +12,16 @@ if [ "${ENABLE_CUSTOM_MODEL_RUNTIME_ENV_DUMP}" = 1 ]; then
     env
 fi
 
+export UV_PROJECT=${CODE_DIR}
+unset UV_COMPILE_BYTECODE
+unset UV_CACHE_DIR
+source ${VENV_PATH}/bin/activate
+# `--active` to install into active kernel venv (instead of creating local from scratch)
+# `--frozen` to skip dependency resolution and just install exactly what's in lock file
+# Compilation DISABLED - kernel venv has already been compiled, and having compilation enabled
+# would re-compile all site-packages (takes quite some time)
+time uv sync --frozen --active --no-progress --group extras || true
+
 echo
 echo "Executing command: drum server $*"
 echo
