183b508cf5cf6da45093e11194921c169ab6fff9
Oleksandr Romaniuk <alexandr.romanyuk@datarobot.com>
2026-02-27 18:36:25 +0200

    drop dead code

--- custom_model_runner/datarobot_drum/drum/root_predictors/utils.py
+++ custom_model_runner/datarobot_drum/drum/root_predictors/utils.py
@@ -22,13 +22,11 @@ from datarobot_drum.drum.enum import (
     PythonArtifacts,
     RArtifacts,
     JavaArtifacts,
-    JuliaArtifacts,
 )
 
 logger = get_drum_logger(__name__)
 
 PYTHON = "python3"
-JULIA = "julia"
 R = "R"
 R_ALL_PREDICT_STRUCTURED_HOOKS = "R_all_predict_structured_hooks"
 R_FIT = "R_fit"
@@ -59,8 +57,6 @@ def _create_custom_model_dir(
         model_template_dir = resources.training_models(language, framework)
         if language == PYTHON:
             files = glob.glob(r"{}/*.py".format(model_template_dir))
-        elif language == JULIA:
-            files = glob.glob(r"{}/*.jl".format(model_template_dir))
         elif language in [R, R_ALL_PREDICT_STRUCTURED_HOOKS, R_FIT]:
             files = glob.glob(r"{}/*.r".format(model_template_dir)) + glob.glob(
                 r"{}/*.R".format(model_template_dir)
@@ -95,7 +91,6 @@ def _create_custom_model_dir(
                         in PythonArtifacts.ALL
                         + RArtifacts.ALL
                         + JavaArtifacts.ALL
-                        + JuliaArtifacts.ALL
                     ):
                         ext = ext.upper()
                     dst = os.path.join(custom_model_dir, f"{name}{ext}")
